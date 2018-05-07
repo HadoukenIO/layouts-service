3fd642195ff09c31407b78cb9bb60a886c956666
Pierre Baize <pierre@openfin.co>
2018-05-07 15:54:03 -0400

    updated deploy script

--- deploy.js
+++ deploy.js
@@ -1,7 +1,7 @@
 const { execSync } = require('child_process');
 const { cdnPath, s3Loc, remoteOrigin, demoPath, s3Root } = require('./deploy.config')
 const distLoc = './dist';
-const demoLoc = './demo';
+const demoLoc = './example/dist';
 const demoCmd = `aws s3 cp ${demoLoc} ${s3Root + demoPath}/ --recursive`;
 const deployCmd = `aws s3 cp ${distLoc} ${s3Loc}/ --recursive`;
 const invalidateCmd = `aws cloudfront create-invalidation --distribution-id E16N7NZUXTHZCF --paths /${cdnPath}/*`;

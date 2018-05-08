533c6c811625a5b29e66018c9d5f642f72ba0c22
Pierre Baize <pierre@openfin.co>
2018-05-08 13:38:59 -0400

    basic styling

--- example/src/frameless.ts
+++ example/src/frameless.ts
@@ -1,5 +1,10 @@
 import {undock} from 'openfin-layouts';
 
+const colors =
+    ['#7B7BFF', '#A7A7A7', '#3D4059', '#D8D8D8', '#1A194D', '#B6B6B6'];
+// tslint:disable-next-line
+const n = parseInt(fin.desktop.Window.getCurrent().name.slice(-1), 10);
+document.body.style.backgroundColor = colors[n];
 const btn = document.createElement('button');
 btn.innerText = 'Undock';
 btn.onclick = () => undock();

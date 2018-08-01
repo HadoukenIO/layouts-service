c66ce2367f0d987eca7eb819fbea31f85743971f
bschwinn <bschwinn@gmail.com>
2018-08-01 17:31:23 -0400

    demo: fix runtime version, start provider when on mac

--- scripts/utils/serve.js
+++ scripts/utils/serve.js
@@ -1,6 +1,8 @@
 const http = require('http')
-
+const os = require('os')
+const path = require('path')
 const handler =require('serve-handler')
+const openfinLauncher = require('hadouken-js-adapter')
 
 const options = {
     public: 'build',
@@ -8,6 +10,13 @@ const options = {
     cleanUrls: false,
 }
 
-module.exports = () => new Promise((resolve, reject) => {
-    http.createServer((req, res) => handler(req, res, options)).listen(1337, resolve)
-})
+module.exports = async () => {
+    await new Promise(async (resolve, reject) => {
+        http.createServer((req, res) => handler(req, res, options)).listen(1337, resolve)
+    });
+    if (os.platform() === 'darwin') {
+        console.log("Starting Provider for Mac OS");
+        const providerConf = path.resolve('build/SnapDockService/app.json');
+        await openfinLauncher.launch({ manifestUrl: providerConf }).catch(err => console.log(err));
+    }
+};

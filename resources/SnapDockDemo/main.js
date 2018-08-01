9b3ef4768be9a437595d877e6ec931372edf8461
Tom Roberts <troberts@openfin.co>
2018-08-01 12:12:38 +0100

    Fixed blank windows when running some tests

--- resources/SnapDockDemo/main.js
+++ resources/SnapDockDemo/main.js
@@ -2,7 +2,7 @@ const colors =
 ['#7B7BFF', '#A7A7A7', '#3D4059', '#D8D8D8', '#1A194D', '#B6B6B6'];
 // tslint:disable-next-line
 const n = parseInt(fin.desktop.Window.getCurrent().name.slice(-1), 10);
-document.body.style.backgroundColor = colors[n - 1];
+document.body.style.backgroundColor = colors[n % 6];
 const h1 = document.createElement('h1');
 h1.innerHTML = `Window ${n}`;
 document.body.appendChild(h1);

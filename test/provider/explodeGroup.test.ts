c0e3389fde77412a32717c6b90535526356ab673
Tom Roberts <troberts@openfin.co>
2018-08-08 10:44:43 +0100

    Extract window initialization into seperate file

--- test/provider/explodeGroup.test.ts
+++ test/provider/explodeGroup.test.ts
@@ -1,20 +1,19 @@
-import { test, GenericTestContext, Context } from 'ava';
-import { dragWindowToOtherWindow, dragSideToSide, Corner } from './utils/dragWindowTo';
+import { test, GenericTestContext, AnyContext } from 'ava';
 import { getBounds } from './utils/getBounds';
-import { createChildWindow } from './utils/createChildWindow';
 import { Window, Fin } from 'hadouken-js-adapter';
 import { getConnection } from './utils/connect';
 import { WindowIdentity } from './utils/undockWindow';
 import { explodeGroup } from './utils/explodeGroup';
 import { isOverlappedWith } from './utils/isOverlappedWith';
 import { isInGroup } from './utils/isInGroup';
-import { Point } from 'hadouken-js-adapter/out/types/src/api/system/point';
+import { WindowInitializer, ArrangementsType } from './utils/WindowInitializer';
 
 // TODO - Change client/service file structure to allow importing these values
 const UNDOCK_MOVE_DISTANCE = 30;
 
 let windows: Window[] = new Array<Window>();
 let fin: Fin;
+let windowInitializer: WindowInitializer;
 
 const windowPositions = [
     { defaultTop: 300, defaultLeft: 300 },
@@ -36,18 +35,92 @@ const windowOptions = {
     frame: false
 };
 
-type MoveArgs = [number, Corner, number, Corner, Point];
-
-type Arrangement = MoveArgs[];
-
-interface ArrangementsType {
-    [numWindows: number]: {
-        [arrangementName: string]: Arrangement
-    };
-}
+/* Encodes the window positions to be tested. The format is: 
+* { 
+*    number of windows: { 
+*        name of window arrangement: [
+*            Array of window movements (executed in sequence)
+*            that will create the desired arrangement of format 
+*            [moved window index, moved corner, target window index, target corner, offset]
+*        ]
+*    }
+* }
+*/
+const arrangements: ArrangementsType = {
+    1: {
+        'default': []
+    },
+    2: {
+        // 1
+        // 2
+        'vertical': [
+            [1, 'top-left', 0, 'bottom-left', { x: 10, y: 2 }],
+        ],
+        // 1 2
+        'horizontal': [
+            [1, 'top-left', 0, 'top-right', { x: 2, y: 10 }],
+        ]
+    },
+    3: {
+        // 1 2 3
+        'line': [
+            [1, 'bottom-left', 0, 'bottom-right', { x: 2, y: -10 }],
+            [2, 'bottom-left', 1, 'bottom-right', { x: 2, y: -10 }],
+        ],
+        //   1
+        // 2
+        //   3
+        'vertical-triangle': [
+            [2, 'top-left', 0, 'bottom-left', { x: 10, y: 2 }],
+            [1, 'top-right', 0, 'top-left', { x: -2, y: 102 }],
+        ],
+        // 1 2
+        //  3
+        'horizontal-triangle': [
+            [1, 'bottom-left', 0, 'bottom-right', { x: 2, y: -10 }],
+            [2, 'top-left', 0, 'bottom-left', { x: 102, y: 2 }],
+        ]
+    },
+    4: {
+        // 1 2
+        // 3 4
+        'square': [
+            [1, 'bottom-left', 0, 'bottom-right', { x: 2, y: -10 }],
+            [2, 'top-right', 0, 'bottom-right', { x: -10, y: 2 }],
+            [3, 'top-left', 0, 'bottom-right', { x: 10, y: 2 }],
+        ]
+    },
+    5: {
+        // 1 2
+        //  3
+        // 4 5
+        'hourglass': [
+            [0, 'top-left', 0, 'top-left', { x: -100, y: -100 }],
+            [1, 'bottom-left', 0, 'bottom-right', { x: 2, y: -10 }],
+            [2, 'top-left', 0, 'bottom-left', { x: 100, y: 2 }],
+            [3, 'top-right', 2, 'bottom-left', { x: 95, y: 2 }],
+            [4, 'top-left', 2, 'bottom-right', { x: -105, y: 2 }],
+        ]
+    },
+    7: {
+        //  1 2
+        // 3 4 5
+        //  6 7
+        'honeycomb': [
+            [0, 'top-left', 0, 'top-left', { x: -100, y: -100 }],
+            [1, 'bottom-left', 0, 'bottom-right', { x: 2, y: -10 }],
+            [2, 'top-left', 0, 'bottom-left', { x: -105, y: 2 }],
+            [3, 'top-left', 2, 'top-right', { x: 2, y: 2 }],
+            [4, 'top-left', 3, 'top-right', { x: 2, y: 2 }],
+            [5, 'top-right', 3, 'bottom-left', { x: 95, y: 2 }],
+            [6, 'top-left', 3, 'bottom-right', { x: -105, y: 2 }],
+        ]
+    }
+};
 
 test.before(async () => {
     fin = await getConnection();
+    windowInitializer = new WindowInitializer(arrangements, windowPositions, windowOptions);
 });
 test.afterEach.always(async () => {
     // Closes all windows and resets the array for the next test.
@@ -59,28 +132,7 @@ test.afterEach.always(async () => {
 
 /* ----- Helper Functions ----- */
 
-async function initWindows(num: number, arrangementName?:string) {
-    await spawnWindows(num);
-
-    if (arrangementName && arrangements.hasOwnProperty(num) && arrangements[num].hasOwnProperty(arrangementName)) {
-        for (const moveArgs of arrangements[num][arrangementName]) {
-            // Destruct the move args before calling it as typescript causes issues otherwise.
-            const [w1,c1,w2,c2,d] = moveArgs;
-            await dragWindowToOtherWindow(windows[w1],c1,windows[w2],c2,d);
-        }
-    }
-}
-
-
-
-async function spawnWindows(num: number) {
-    for (let i = 0; i < num; i++) {
-        windows[i] = await createChildWindow({ ...(windowPositions[i]), ...windowOptions });
-    }
-}
-
-// tslint:disable-next-line:no-any
-async function assertExploded(t: GenericTestContext<Context<any>>) {
+async function assertExploded(t: GenericTestContext<AnyContext>) {
 
     // Check each window
     for (let i = 0; i < windows.length; i++) {
@@ -104,120 +156,37 @@ async function assertExploded(t: GenericTestContext<Context<any>>) {
 
 /* ----- Individual Tests ----- */
 
-test('One ungrouped window - no effect on explode', async t => {
-    await spawnWindows(1);
-
-    // Slight delay to ensure window is completely loaded
-    await new Promise(r => setTimeout(r, 1000));
-
-    const boundsBefore = await getBounds(windows[0]);
-
-    await explodeGroup(windows[0].identity as WindowIdentity);
-
-    const boundsAfter = await getBounds(windows[0]);
-
-    // Check window did not move in any way
-    t.deepEqual(boundsBefore, boundsAfter, 'Single window moved during explode');
-
-    // Runs multiple assertions checking that the group is exploded. Somewhat irrelevant in this case, but run it anyway as a smoke test.
-    await assertExploded(t);
-});
+/*
+* Add any individual tests to be run here.
+*/
 
 /* ----- Macro Tests ----- */
 
-/*  A basic explode test will be run for each window arrangement in this object.
-* To add an additional test, simply add a new entry. The format is: 
-* { 
-*    number of windows: { 
-*        name of window arrangement: [
-*            Array of window movements (executed in sequence)
-*            that will create the desired arrangement of format 
-*            [moved window index, moved corner, target window index, target corner, offset]
-*        ]
-*    }
-* }
+/* 
+* A basic explode test will be run for each window arrangement in the arrangemtns object.
+* To add an additional window layouts to be tested, simply add new entries there. 
 */
-const arrangements:ArrangementsType = {
-    2: {
-        // 1
-        // 2
-        'vertical': [
-            [1, 'top-left', 0, 'bottom-left', { x: 10, y: 2 }],
-        ], 
-        // 1 2
-        'horizontal': [
-            [1, 'top-left', 0, 'top-right', { x: 2, y: 10 }],
-        ]
-    },
-    3: {
-        // 1 2 3
-        'line': [
-            [1, 'bottom-left', 0, 'bottom-right', { x: 2, y: -10 }],
-            [2, 'bottom-left', 1, 'bottom-right', { x: 2, y: -10 }],
-        ],
-        //   1
-        // 2
-        //   3
-        'vertical-triangle': [
-            [2, 'top-left', 0, 'bottom-left', { x: 10, y: 2 }],
-            [1, 'top-right', 0, 'top-left', { x: -2, y: 102 }],
-        ],
-        // 1 2
-        //  3
-        'horizontal-triangle': [
-            [1, 'bottom-left', 0, 'bottom-right', { x: 2, y: -10 }],
-            [2, 'top-left', 0, 'bottom-left', {x: 102, y: 2}],
-        ]
-    },
-    4: {
-        // 1 2
-        // 3 4
-        'square': [
-            [1, 'bottom-left', 0, 'bottom-right', { x: 2, y: -10 }],
-            [2, 'top-right', 0, 'bottom-right', { x: -10, y: 2 }],
-            [3, 'top-left', 0, 'bottom-right', { x: 10, y: 2 }],
-        ]
-    },
-    5: {
-        // 1 2
-        //  3
-        // 4 5
-        'hourglass': [
-            [0, 'top-left', 0, 'top-left', {x: -100, y: -100}],
-            [1, 'bottom-left', 0, 'bottom-right', { x: 2, y: -10 }],
-            [2, 'top-left', 0, 'bottom-left', {x: 100, y: 2}],
-            [3, 'top-right', 2, 'bottom-left', {x:95, y:2}],
-            [4, 'top-left', 2, 'bottom-right', {x:-105, y:2}],
-        ]
-    },
-    7: {
-        //  1 2
-        // 3 4 5
-        //  6 7
-        'honeycomb': [
-            [0, 'top-left', 0, 'top-left', {x: -100, y: -100}],
-            [1, 'bottom-left', 0, 'bottom-right', { x: 2, y: -10 }],
-            [2, 'top-left', 0, 'bottom-left', {x: -105, y: 2}],
-            [3, 'top-left', 2, 'top-right', {x:2, y:2}],
-            [4, 'top-left', 3, 'top-right', {x:2, y:2}],
-            [5, 'top-right', 3, 'bottom-left', {x:95, y:2}],
-            [6, 'top-left', 3, 'bottom-right', {x:-105, y:2}],
-        ]
-    }
-};
-
 Object.keys(arrangements).forEach(num => {
     const count = Number.parseInt(num);
-    
+
     Object.keys(arrangements[count]).forEach(name => {
         test(`${count} windows - ${name}`, async t => {
 
-            await initWindows(count, name);
+            // This will spawn the required number of windows in the correct positions/groups
+            windows = await windowInitializer.initWindows(count, name);
 
-            await explodeGroup(windows[0].identity as WindowIdentity);
+            // Special handling for single window. Checks window did not move in any way
+            if (count === 1) {
+                const boundsBefore = await getBounds(windows[0]);
+                await explodeGroup(windows[0].identity as WindowIdentity);
+                const boundsAfter = await getBounds(windows[0]);
+                t.deepEqual(boundsBefore, boundsAfter, 'Single window moved during explode');
+            } else {
+                await explodeGroup(windows[0].identity as WindowIdentity);
+            }
 
+            // Runs multiple tests to ensure that the group has succesfully exploded.
             await assertExploded(t);
-
         });
     });
 });

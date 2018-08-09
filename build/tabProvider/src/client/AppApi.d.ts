6934083d99959601176f16a5e4daad8b67c2cc79
Philip Broadbent <>
2018-08-09 15:52:16 -0400

    Removing build directory

--- build/tabProvider/src/client/AppApi.d.ts
+++
@@ -1,7 +0,0 @@
-import { Api } from "./Api";
-export declare class AppApi extends Api {
-    private _ID;
-    constructor();
-    init(url?: string | undefined, height?: number | undefined): void;
-    deregister(): void;
-}

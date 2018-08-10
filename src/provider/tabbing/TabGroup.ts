fd018248d69de1631ae457d4e14cda865b84aec7
ryanpetersonOF <ryan@openfin.co>
2018-08-10 09:46:15 +0100

    Replaced uuid module with function (#1)
    Removed UUID module as a dependency. Replaced with a single function.

--- src/provider/tabbing/TabGroup.ts
+++ src/provider/tabbing/TabGroup.ts
@@ -1,4 +1,4 @@
-import { v4 as uuidv4 } from "uuid";
+import { uuidv4 } from "./TabUtilities";
 import { TabApiEvents } from "../../shared/APITypes";
 import { TabIdentifier, TabPackage, TabWindowOptions } from "../../shared/types";
 import { GroupWindow } from "./GroupWindow";

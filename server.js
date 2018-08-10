654aa11be1eb93d1b6a4065ad54a73286a71ddad
Philip Broadbent <>
2018-08-09 21:54:15 -0400

    Changed how provider is started - now launched as service rather than through server.js. Removed old build script. Removed dist folders from .gitignore as now no longer used.

--- server.js
+++ server.js
@@ -1,35 +1,3 @@
-//const openfinLauncher = require("openfin-launcher");
-//const path = require("path");
-//const express = require("express");
-//const http = require("http");
-//const app = express();
-
-//const configPath1 = path.resolve("./app.json");
-//const configPath2 = path.resolve("./src/demo/app.json");
-////Update our config and launch openfin.
-//function launchOpenFin(configPath) {
-//	openfinLauncher.launchOpenFin({ configPath: configPath }).catch(err => console.log(err));
-//}
-///*
-////Start the server server and launch our app.
-//*/
-
-//app.use(express.static("./src"));
-
-//try {
-//	http.createServer(app).listen(9001, () => {
-//		console.log("Server Created!");
-//		console.log("Starting Openfin...");
-//		launchOpenFin(configPath1);
-//		launchOpenFin(configPath2);
-//	});
-//} catch (e) {
-//	launchOpenFin();
-//}
-
-
-
-
 const { launch } = require('hadouken-js-adapter');
 const express = require('express');
 const os = require('os');
@@ -48,8 +16,6 @@ app.listen(1337, async () => {
         const providerConf = path.resolve('build/provider/app.json');
         await launch({ manifestUrl: providerConf }).catch(console.log);
     }
-
-    launch({ manifestUrl: "http://localhost:1337/tabProvider/app.json" }).catch(console.log);
 
     console.log("Launching demo application")
     await launch({ manifestUrl: "http://localhost:1337/demo/tabbing/app.json" }).catch(console.log);

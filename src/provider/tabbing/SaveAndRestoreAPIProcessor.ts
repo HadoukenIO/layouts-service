6cb9ba5ac02c46e5590525849a42cc1f3c6e6a72
Ryan Peterson <ryan@openfin.co>
2018-08-14 12:19:41 +0100

    Cleanup save and restore gather function

--- src/provider/tabbing/SaveAndRestoreAPIProcessor.ts
+++ src/provider/tabbing/SaveAndRestoreAPIProcessor.ts
@@ -1,80 +1,43 @@
-import { SaveAndRestoreActions, SaveAndRestoreEvents } from "../../client/APITypes";
 import { TabBlob, TabIdentifier } from "../../client/types";
 import { Tab } from "./Tab";
 import { TabGroup } from "./TabGroup";
 import { TabService } from "./TabService";
 
 /**
- * Handles all calls from tab api to service
+ * Gathers information from tab sets and their tabs, and returns as a JSON object back to the requesting application/window.
+ * @param uuid Uuid of the requesting Application
+ * @param name Name of the requesting window
+ * @returns {TabBlob[] | undefined} Returns undefined if a tab service is not around.
  */
-export class SaveAndRestoreAPIProcessor {
-	/**
-	 * The tab service itself
-	 */
-	private mTabService: TabService;
+export async function getTabSaveInfo(): Promise<TabBlob[] | undefined> {
 
-	/**
-	 * @constructor Constructor for the SaveAndRestoreAPIProcessor
-	 */
-	constructor(service: TabService) {
-		this.mTabService = service;
+	if (!TabService.INSTANCE) {
+		console.error("No Tab Service!");
+		return;
 	}
+	return Promise.all(
+		TabService.INSTANCE.tabGroups.map(async (group: TabGroup) => {
+			const tabs: TabIdentifier[] = group.tabs.map((tab: Tab) => {
+				return tab.ID;
+			});
 
-	/**
-	 * Initialises the SaveAndRestoreAPIProcessor
-	 */
-	public init(): void {
-		fin.desktop.InterApplicationBus.subscribe("*", "SAR-API", this.process.bind(this));
-	}
+			const [groupBounds, appBounds] = await Promise.all([group.window.getWindowBounds(), group.activeTab.window.getWindowBounds()]);
 
-	/**
-	 * Processes incoming API messages from the Tab API.
-	 * @param message The payload the tab api sent
-	 * @param uuid uuid of the sender
-	 * @param name name of the sender
-	 */
-	private process(message: { action: SaveAndRestoreActions }, uuid: string, name: string): void {
-		switch (message.action) {
-			case SaveAndRestoreActions.GETBLOB: {
-				this._onGetBlob(uuid, name);
-				break;
-			}
-			default:
-				console.error(message.action, " Not Implemented!");
-				break;
-		}
-	}
+			const groupInfo = {
+				url: group.window.initialWindowOptions.url!,
+				active: group.activeTab.ID,
+				dimensions: {
+					x: groupBounds.left!,
+					y: groupBounds.top!,
+					width: groupBounds.width!,
+					setHeight: groupBounds.height!,
+					appHeight: appBounds.height!
+				}
+			};
 
-	/**
-	 * Gathers information from tab sets and their tabs, and returns as a JSON object back to the requesting application/window.
-	 * @param uuid Uuid of the requesting Application
-	 * @param name Name of the requesting window
-	 */
-	private async _onGetBlob(uuid: string, name: string): Promise<void> {
-		const tabBlobs: TabBlob[] = await Promise.all(
-			this.mTabService.tabGroups.map(async (group: TabGroup) => {
-				const tabs: TabIdentifier[] = group.tabs.map((tab: Tab) => {
-					return tab.ID;
-				});
+			return { tabs, groupInfo };
+		})
+	);
 
-				const [groupBounds, appBounds] = await Promise.all([group.window.getWindowBounds(), group.activeTab.window.getWindowBounds()]);
-
-				const groupInfo = {
-					url: group.window.initialWindowOptions.url!,
-					active: group.activeTab.ID,
-					dimensions: {
-						x: groupBounds.left!,
-						y: groupBounds.top!,
-						width: groupBounds.width!,
-						setHeight: groupBounds.height!,
-						appHeight: appBounds.height!
-					}
-				};
-
-				return { tabs, groupInfo };
-			})
-		);
-
-		fin.desktop.InterApplicationBus.send(uuid, name, SaveAndRestoreEvents.GETBLOBRETURN, tabBlobs);
-	}
 }
+

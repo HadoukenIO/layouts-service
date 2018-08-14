23383f75a82d8b718a25c7578200324360192648
Tom Roberts <troberts@openfin.co>
2018-08-14 12:32:02 +0100

    Allow reordering of tab handles in tabstrip UI

--- src/provider/tabbing/tabstrip/TabItem.ts
+++ src/provider/tabbing/tabstrip/TabItem.ts
@@ -1,213 +1,220 @@
-import { TabIdentifier, TabProperties } from "../../../client/types";
-import { TabManager } from "./TabManager";
+import {TabIdentifier, TabProperties} from '../../../client/types';
+import {TabManager} from './TabManager';
 
 export class Tab {
-	/**
-	 * Contains the HTML Element for the tab.
-	 */
-	private _domNode!: HTMLElement;
+    /**
+     * Contains the HTML Element for the tab.
+     */
+    private _domNode!: HTMLElement;
 
-	/**
-	 * Properties to the tab (icon, title)
-	 */
-	private _properties: TabProperties;
+    /**
+     * Properties to the tab (icon, title)
+     */
+    private _properties: TabProperties;
 
-	/**
-	 * ID of the Tab (uuid, name);
-	 */
-	private _ID: TabIdentifier;
+    /**
+     * ID of the Tab (uuid, name);
+     */
+    private _ID: TabIdentifier;
 
-	/**
-	 * Constructor for the Tab class.
-	 * @param {TabIdentifier} tabID An object containing the uuid, name for the external application/window.
-	 */
-	constructor(tabID: TabIdentifier, tabProperties: TabProperties) {
-		this._ID = tabID;
-		this._properties = tabProperties;
-	}
+    /**
+     * Constructor for the Tab class.
+     * @param {TabIdentifier} tabID An object containing the uuid, name for the external application/window.
+     */
+    constructor(tabID: TabIdentifier, tabProperties: TabProperties) {
+        this._ID = tabID;
+        this._properties = tabProperties;
+    }
 
-	/**
-	 * Initializes the Tab class
-	 */
-	public init() {
-		this._render();
-	}
+    /**
+     * Initializes the Tab class
+     */
+    public init() {
+        this._render();
+    }
 
-	/**
-	 * Removes the Tab from DOM.
-	 */
-	public remove(): void {
-		this._domNode.remove();
-	}
+    /**
+     * Removes the Tab from DOM.
+     */
+    public remove(): void {
+        this._domNode.remove();
+    }
 
-	/**
-	 * Sets the Active class on the Tab DOM.
-	 */
-	public setActive(): void {
-		this._domNode.classList.add("active");
-	}
+    /**
+     * Sets the Active class on the Tab DOM.
+     */
+    public setActive(): void {
+        this._domNode.classList.add('active');
+    }
 
-	/**
-	 * Removes the Active class from the Tab DOM.
-	 */
-	public unsetActive(): void {
-		this._domNode.classList.remove("active");
-	}
+    /**
+     * Removes the Active class from the Tab DOM.
+     */
+    public unsetActive(): void {
+        this._domNode.classList.remove('active');
+    }
 
-	/**
-	 * Updates the icon of this tab.
-	 * @param {string} icon The URL to the icon image.
-	 */
-	public updateIcon(icon = ""): void {
-		const iconNode = this._domNode.querySelectorAll(".tab-favicon")[0];
-		(iconNode as HTMLElement).style.backgroundImage = `url("${icon}")`;
+    /**
+     * Updates the icon of this tab.
+     * @param {string} icon The URL to the icon image.
+     */
+    public updateIcon(icon = ''): void {
+        const iconNode = this._domNode.querySelectorAll('.tab-favicon')[0];
+        (iconNode as HTMLElement).style.backgroundImage = `url("${icon}")`;
 
-		this._properties.icon = icon;
-	}
+        this._properties.icon = icon;
+    }
 
-	/**
-	 * Updates the text of the tab.
-	 * @param {string} text Text to update with.
-	 */
-	public updateText(text: string): void {
-		const textNode = this._domNode.querySelectorAll(".tab-content")[0];
-		(textNode as HTMLElement).textContent = text;
+    /**
+     * Updates the text of the tab.
+     * @param {string} text Text to update with.
+     */
+    public updateText(text: string): void {
+        const textNode = this._domNode.querySelectorAll('.tab-content')[0];
+        (textNode as HTMLElement).textContent = text;
 
-		this._properties.title = text;
-	}
-	/**
-	 * Handles the HTML5 DragEvent onStart
-	 * @param {DragEvent} e DragEvent
-	 */
-	private _onDragStart(e: DragEvent): void {
-		e.dataTransfer.effectAllowed = "move";
+        this._properties.title = text;
+    }
+    /**
+     * Handles the HTML5 DragEvent onStart
+     * @param {DragEvent} e DragEvent
+     */
+    private _onDragStart(e: DragEvent): boolean {
+        e.dataTransfer.effectAllowed = 'move';
 
-		TabManager.tabAPI.startDrag();
-	}
+        TabManager.tabAPI.startDrag();
+        return true;
+    }
 
-	/**
-	 * Handles the HTML5 DragEvent onDragEnd
-	 * @param {DragEvent} e DragEvent
-	 */
-	private _onDragEnd(e: DragEvent): void {
-		// @ts-ignore
-		TabManager.tabAPI.endDrag(e, this._ID.uuid, this._ID.name);
-	}
+    /**
+     * Handles the HTML5 DragEvent onDragEnd
+     * @param {DragEvent} e DragEvent
+     */
+    private _onDragEnd(e: DragEvent): void {
+        // @ts-ignore
+        TabManager.tabAPI.endDrag(e, this._ID.uuid, this._ID.name);
+    }
 
-	/**
-	 * Renders the Tab to the DOM from generation.
-	 */
-	private _render(): void {
-		this._domNode = this._generateDOM();
-		TabManager.tabContainer.appendChild(this._domNode);
-		this.updateText(this._properties.title!);
-		this.updateIcon(this._properties.icon!);
-	}
+    /**
+     * Renders the Tab to the DOM from generation.
+     */
+    private _render(): void {
+        this._domNode = this._generateDOM();
+        TabManager.tabContainer.appendChild(this._domNode);
+        this.updateText(this._properties.title!);
+        this.updateIcon(this._properties.icon!);
+    }
 
-	/**
-	 * Handles all click events from this Tab DOM.
-	 * @param {MouseEvent} e MouseEvent
-	 */
-	private _onClickHandler(e: MouseEvent): void {
-		switch ((e.target as Element).className) {
-			case "tab-exit": {
-				TabManager.tabAPI.closeTab(this._ID.uuid, this._ID.name);
+    /**
+     * Handles all mouseDown events from this Tab DOM.
+     * @param {MouseEvent} e MouseEvent
+     */
+    private _onMouseDownHandler(e: MouseEvent): void {
+        this.setActive();
+        TabManager.tabAPI.activateTab(this._ID.uuid, this._ID.name);
+    }
 
-				break;
-			}
-			default: {
-				TabManager.tabAPI.activateTab(this._ID.uuid, this._ID.name);
-			}
-		}
-	}
 
-	/**
-	 * Handles all double click events from this Tab DOM.
-	 * @param {MouseEvent} e MouseEvent
-	 */
-	private _onDblClickHandler(e: MouseEvent): void {
-		switch ((e.target as Element).className) {
-			case "tab-content":
-			case "tab-content-wrap": {
-				this._handlePropertiesInput();
-				break;
-			}
-			default: {
-				// @ts-ignore
-				window.Tab.activateTab(this._ID.uuid, this._ID.name);
-			}
-		}
-	}
+    /**
+     * Handles all click events from this Tab DOM.
+     * @param {MouseEvent} e MouseEvent
+     */
+    private _onClickHandler(e: MouseEvent): void {
+        switch ((e.target as Element).className) {
+            case 'tab-exit': {
+                TabManager.tabAPI.closeTab(this._ID.uuid, this._ID.name);
 
-	/**
-	 * Generates the DOM for this tab.
-	 * @returns {HTMLElement} DOM Node
-	 */
-	private _generateDOM(): HTMLElement {
-		// Get tab template from HTML (index.html)
-		const tabTemplate: HTMLTemplateElement = (document.getElementById("tab-template") as HTMLTemplateElement)!;
-		const tabTemplateDocFrag: DocumentFragment = document.importNode(tabTemplate.content, true);
-		const tab: HTMLElement = tabTemplateDocFrag.firstElementChild as HTMLElement;
+                break;
+            }
+            default: { TabManager.tabAPI.activateTab(this._ID.uuid, this._ID.name); }
+        }
+    }
 
-		// Set the onclick, drag events to top tab DOM.
-		tab.onclick = this._onClickHandler.bind(this);
-		tab.ondblclick = this._onDblClickHandler.bind(this);
-		tab.addEventListener("dragstart", this._onDragStart.bind(this), false);
+    /**
+     * Handles all double click events from this Tab DOM.
+     * @param {MouseEvent} e MouseEvent
+     */
+    private _onDblClickHandler(e: MouseEvent): void {
+        switch ((e.target as Element).className) {
+            case 'tab-content':
+            case 'tab-content-wrap': {
+                this._handlePropertiesInput();
+                break;
+            }
+            default: {
+                // @ts-ignore
+                window.Tab.activateTab(this._ID.uuid, this._ID.name);
+            }
+        }
+    }
 
-		tab.addEventListener("dragend", this._onDragEnd.bind(this), false);
+    /**
+     * Generates the DOM for this tab.
+     * @returns {HTMLElement} DOM Node
+     */
+    private _generateDOM(): HTMLElement {
+        // Get tab template from HTML (index.html)
+        const tabTemplate: HTMLTemplateElement = (document.getElementById('tab-template') as HTMLTemplateElement)!;
+        const tabTemplateDocFrag: DocumentFragment = document.importNode(tabTemplate.content, true);
+        const tab: HTMLElement = tabTemplateDocFrag.firstElementChild as HTMLElement;
 
-		return tab;
-	}
+        // Set the onclick, drag events to top tab DOM.
+        tab.onclick = this._onClickHandler.bind(this);
+        tab.ondblclick = this._onDblClickHandler.bind(this);
+        tab.onmousedown = this._onMouseDownHandler.bind(this);
+        tab.addEventListener('dragstart', this._onDragStart.bind(this), true);
+        tab.addEventListener('dragend', this._onDragEnd.bind(this), true);
 
-	/**
-	 * Creates the input field on the tab and handles events on it.
-	 */
-	private _handlePropertiesInput(): void {
-		const textNode: Element = this._domNode.querySelectorAll(".tab-content")[0];
-		const textNodeValue: string | null = textNode.textContent;
-		textNode.textContent = "";
+        return tab;
+    }
 
-		const inputNode: HTMLInputElement = document.createElement("input");
+    /**
+     * Creates the input field on the tab and handles events on it.
+     */
+    private _handlePropertiesInput(): void {
+        const textNode: Element = this._domNode.querySelectorAll('.tab-content')[0];
+        const textNodeValue: string|null = textNode.textContent;
+        textNode.textContent = '';
 
-		inputNode.value = textNodeValue || "";
+        const inputNode: HTMLInputElement = document.createElement('input');
 
-		function _onBlur(): void {
-			try {
-				inputNode.remove();
-				// @ts-ignore
-				TabManager.tabAPI.updateTabProperties(this._ID.uuid, this._ID.name, { title: inputNode.value });
-			} catch (e) {}
-		}
+        inputNode.value = textNodeValue || '';
 
-		inputNode.addEventListener("keypress", keyEvent => {
-			const key = keyEvent.which || keyEvent.keyCode;
-			if (key === 13) {
-				_onBlur();
-			}
-		});
+        function _onBlur(): void {
+            try {
+                inputNode.remove();
+                // @ts-ignore
+                TabManager.tabAPI.updateTabProperties(this._ID.uuid, this._ID.name, {title: inputNode.value});
+            } catch (e) {
+            }
+        }
 
-		inputNode.addEventListener("blur", _onBlur.bind(this));
+        inputNode.addEventListener('keypress', keyEvent => {
+            const key = keyEvent.which || keyEvent.keyCode;
+            if (key === 13) {
+                _onBlur();
+            }
+        });
 
-		textNode.insertAdjacentElement("afterbegin", inputNode);
-		inputNode.focus();
-	}
+        inputNode.addEventListener('blur', _onBlur.bind(this));
 
-	/**
-	 * Returns tab identifier object consisting of UUID, Name
-	 * @returns {TabIdentifier} {uuid, name}
-	 */
-	public get ID(): TabIdentifier {
-		return {
-			uuid: this._ID.uuid,
-			name: this._ID.name
-		};
-	}
+        textNode.insertAdjacentElement('afterbegin', inputNode);
+        inputNode.focus();
+    }
 
-	/**
-	 * Returns the DOM Node for the tab
-	 * @returns {HTMLElement} DOM Node
-	 */
-	public get DOM(): HTMLElement {
-		return this._domNode;
-	}
+    /**
+     * Returns tab identifier object consisting of UUID, Name
+     * @returns {TabIdentifier} {uuid, name}
+     */
+    public get ID(): TabIdentifier {
+        return {uuid: this._ID.uuid, name: this._ID.name};
+    }
+
+    /**
+     * Returns the DOM Node for the tab
+     * @returns {HTMLElement} DOM Node
+     */
+    public get DOM(): HTMLElement {
+        return this._domNode;
+    }
 }
--- src/provider/tabbing/tabstrip/TabStrip.ts
+++ src/provider/tabbing/tabstrip/TabStrip.ts
@@ -1,19 +1,16 @@
-import { TabManager } from "./TabManager";
-
-// tslint:disable-next-line:no-any
-declare var fin: any;
+import {TabManager} from './TabManager';
 
 fin.desktop.main(() => {
-	const tabManager: TabManager = new TabManager();
-	(window as Window & { tabManager: TabManager }).tabManager = tabManager;
+    const tabManager: TabManager = new TabManager();
+    (window as Window & {tabManager: TabManager}).tabManager = tabManager;
 
-	const minimizeElem: HTMLElement | null = document.getElementById("window-button-minimize");
-	const maximizeElem: HTMLElement | null = document.getElementById("window-button-maximize");
-	const closeElem: HTMLElement | null = document.getElementById("window-button-exit");
+    const minimizeElem: HTMLElement|null = document.getElementById('window-button-minimize');
+    const maximizeElem: HTMLElement|null = document.getElementById('window-button-maximize');
+    const closeElem: HTMLElement|null = document.getElementById('window-button-exit');
 
-	if (TabManager.tabAPI && TabManager.tabAPI.windowActions) {
-		minimizeElem!.onclick = TabManager.tabAPI.windowActions.minimize;
-		maximizeElem!.onclick = TabManager.tabAPI.windowActions.toggleMaximize;
-		closeElem!.onclick = TabManager.tabAPI.windowActions.close;
-	}
+    if (TabManager.tabAPI && TabManager.tabAPI.windowActions) {
+        minimizeElem!.onclick = TabManager.tabAPI.windowActions.minimize;
+        maximizeElem!.onclick = TabManager.tabAPI.windowActions.toggleMaximize;
+        closeElem!.onclick = TabManager.tabAPI.windowActions.close;
+    }
 });

21cf2d91a715cd700d1362a3e6a81f7d90047c92
Ryan Peterson <ryan@openfin.co>
2018-08-23 13:34:58 +0100

    Add Restore Tabs S&R API

--- src/provider/tabbing/SaveAndRestoreAPI.ts
+++ src/provider/tabbing/SaveAndRestoreAPI.ts
@@ -1,7 +1,8 @@
-import { TabBlob, TabIdentifier } from "../../client/types";
-import { Tab } from "./Tab";
-import { TabGroup } from "./TabGroup";
-import { TabService } from "./TabService";
+import {TabBlob, TabIdentifier} from '../../client/types';
+import {Tab} from './Tab';
+import {TabGroup} from './TabGroup';
+import {TabService} from './TabService';
+import {createTabGroupsFromMultipleWindows} from './TabUtilities';
 
 /**
  * Gathers information from tab sets and their tabs, and returns as a JSON object back to the requesting application/window.
@@ -9,44 +10,38 @@ import { TabService } from "./TabService";
  * @param name Name of the requesting window
  * @returns {TabBlob[] | undefined} Returns undefined if a tab service is not around.
  */
-export async function getTabSaveInfo(): Promise<TabBlob[] | undefined> {
+export async function getTabSaveInfo(): Promise<TabBlob[]|undefined> {
+    if (!TabService.INSTANCE) {
+        console.error('No Tab Service!');
+        return;
+    }
 
-	if (!TabService.INSTANCE) {
-		console.error("No Tab Service!");
-		return;
-	}
+    return Promise.all(TabService.INSTANCE.tabGroups.map(async (group: TabGroup) => {
+        const tabs: TabIdentifier[] = group.tabs.map((tab: Tab) => {
+            return tab.ID;
+        });
 
-	return Promise.all(
-		TabService.INSTANCE.tabGroups.map(async (group: TabGroup) => {
-			const tabs: TabIdentifier[] = group.tabs.map((tab: Tab) => {
-				return tab.ID;
-			});
+        const [groupBounds, appBounds] = await Promise.all([group.window.getWindowBounds(), group.activeTab.window.getWindowBounds()]);
 
-			const [groupBounds, appBounds] = await Promise.all([group.window.getWindowBounds(), group.activeTab.window.getWindowBounds()]);
+        const groupInfo = {
+            url: group.window.initialWindowOptions.url!,
+            active: group.activeTab.ID,
+            dimensions:
+                {x: groupBounds.left!, y: groupBounds.top!, width: groupBounds.width!, tabGroupHeight: groupBounds.height!, appHeight: appBounds.height!}
+        };
 
-			const groupInfo = {
-				url: group.window.initialWindowOptions.url!,
-				active: group.activeTab.ID,
-				dimensions: {
-					x: groupBounds.left!,
-					y: groupBounds.top!,
-					width: groupBounds.width!,
-					tabGroupHeight: groupBounds.height!,
-					appHeight: appBounds.height!
-				}
-			};
-
-			return { tabs, groupInfo };
-		})
-	);
-
+        return {tabs, groupInfo};
+    }));
 }
 
 /**
  * Restores tabs and tab groups using the given tab blob information.
  * @param {TabBlob[]} tabBlob Array of TabBlobs
  */
-export function restoreTabs(tabBlob: TabBlob[]): void {
-	// TODO add restore tabs here
-}
+export function restoreTabs(tabBlob: TabBlob[]): Promise<void> {
+    if (!TabService.INSTANCE) {
+        return Promise.reject('No Tab Service!');
+    }
 
+    return createTabGroupsFromMultipleWindows(tabBlob);
+}

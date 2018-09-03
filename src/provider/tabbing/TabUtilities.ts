e3c4d360640a092c4498b8af81b09629c1854d0f
ryanpetersonOF <ryan@openfin.co>
2018-09-03 14:16:46 +0100

    linting

--- src/provider/tabbing/TabUtilities.ts
+++ src/provider/tabbing/TabUtilities.ts
@@ -129,8 +129,6 @@ export async function createTabGroupsFromTabBlob(tabBlob: TabBlob[]): Promise<vo
         await group.realignApps();
         await group.switchTab({uuid: blob.groupInfo.active.uuid, name: blob.groupInfo.active.name});
     }
-
-   
 }
 
 (window as Window & {createTabGroupsFromTabBlob: Function}).createTabGroupsFromTabBlob = createTabGroupsFromTabBlob;

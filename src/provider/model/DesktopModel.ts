7350758878da9ac075da206cbbc3d5299dcdb92d
ryanpetersonOF <ryan@openfin.co>
2018-10-04 10:54:04 +0100

    MouseTracker

--- src/provider/model/DesktopModel.ts
+++ src/provider/model/DesktopModel.ts
@@ -7,6 +7,7 @@ import {RectUtils} from '../snapanddock/utils/RectUtils';
 import {DesktopTabGroup} from './DesktopTabGroup';
 import {DesktopWindow, WindowIdentity, WindowState} from './DesktopWindow';
 import {ZIndexer} from './ZIndexer';
+import { MouseTracker } from './MouseTracker';
 
 export class DesktopModel {
     private windows: DesktopWindow[];
@@ -14,7 +15,7 @@ export class DesktopModel {
     private snapGroups: DesktopSnapGroup[];
     private windowLookup: {[key: string]: DesktopWindow};
     private zIndexer: ZIndexer;
-
+    private mouseTracker: MouseTracker;
     private pendingRegistrations: WindowIdentity[];
 
     constructor() {
@@ -23,6 +24,7 @@ export class DesktopModel {
         this.snapGroups = [];
         this.windowLookup = {};
         this.zIndexer = new ZIndexer(this);
+        this.mouseTracker = new MouseTracker();
         this.pendingRegistrations = [];
 
         DesktopWindow.onCreated.add(this.onWindowCreated, this);
@@ -73,6 +75,10 @@ export class DesktopModel {
 
     public getId(identity: WindowIdentity): string {
         return `${identity.uuid}/${identity.name}`;
+    }
+
+    public getMouseTracker(): MouseTracker {
+        return this.mouseTracker;
     }
 
     public getWindow(identity: WindowIdentity): DesktopWindow|null {
--- src/provider/model/MouseTracker.ts
+++ src/provider/model/MouseTracker.ts
@@ -1,3 +1,40 @@
-export class MousePoller {
-    
+import { PointTopLeft } from "hadouken-js-adapter/out/types/src/api/system/point";
+import { DesktopWindow, WindowState } from "./DesktopWindow";
+
+export class MouseTracker {
+    private window: DesktopWindow | null = null;
+    private mouseOffset: PointTopLeft | null = null;
+
+    /**
+     * Initializes the mouse tracking process relative to a window.
+     * @param {DesktopWindow} window The window to use as a reference point for calculations.  Typically the window being dragged.
+     */
+    public async start(window: DesktopWindow){
+        const mousePosition: PointTopLeft = await fin.System.getMousePosition();
+        const windowState: WindowState = window.getState();
+
+        this.window = window;
+        this.mouseOffset = {left: mousePosition.left - (windowState.center.x - windowState.halfSize.x), top: mousePosition.top - (windowState.center.y - windowState.halfSize.y)};
+    }
+
+    /**
+     * Returns the mouse position on screen when a window is being moved. If no window is being moved then we return null.
+     * @returns {PointTopLeft | null} Mouse Position or null.
+     */
+    public getPos(): PointTopLeft | null {
+        if(this.window && this.mouseOffset){
+            const currentWindowState: WindowState = this.window.getState();
+
+            return {left: this.mouseOffset.left + (currentWindowState.center.x - currentWindowState.halfSize.x), top: this.mouseOffset.top + (currentWindowState.center.y - currentWindowState.halfSize.y)}
+        }
+
+        return null;
+    }
+
+    /**
+     * Ends the mouse tracking process.
+     */
+    public end() {
+        this.window = this.mouseOffset = null;
+    }
 }

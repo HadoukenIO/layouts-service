0d7fd1b726b9478f02848b8761a143da37b1ab9e
ryanpetersonOF <ryan@openfin.co>
2018-10-15 14:54:11 +0100

    linting fix

--- src/provider/model/DesktopTabGroup.ts
+++ src/provider/model/DesktopTabGroup.ts
@@ -21,8 +21,8 @@ export class DesktopTabGroup {
      * Sourced from https://stackoverflow.com/questions/105034/create-guid-uuid-in-javascript
      */
     private static createTabGroupId(): string {
-        //@ts-ignore Black Magic
         return 'TABSET-' +
+            //@ts-ignore Black Magic
             ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c => (c ^ (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))).toString(16));
     }
 

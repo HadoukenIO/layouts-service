6af6ad6e859b24c712840b9ef64fa44c4099dfd9
Tom Roberts <troberts@openfin.co>
2018-10-17 14:36:32 +0100

    Added basic tests for disabled docking

---
+++ test/provider/dockingDisabled.test.ts
@@ -0,0 +1,96 @@
+import {test} from 'ava';
+import { getConnection } from './utils/connect';
+import { createChildWindow } from './utils/createChildWindow';
+import { Fin, Window } from 'hadouken-js-adapter';
+import { promiseMap } from '../../src/provider/snapanddock/utils/async';
+import { dragSideToSide, dragWindowTo } from './utils/dragWindowTo';
+import { assertGrouped, assertNotGrouped } from './utils/assertions';
+import { delay } from './utils/delay';
+import { getBounds, NormalizedBounds } from './utils/getBounds';
+
+let windows: Window[] = new Array<Window>(2);
+let fin: Fin;
+
+const windowOptions = [{
+    autoShow: true,
+    saveWindowState: false,
+    defaultTop: 100,
+    defaultLeft: 100,
+    defaultHeight: 200,
+    defaultWidth: 200,
+    url: 'http://localhost:1337/demo/frameless-window.html',
+    frame: false
+},{
+    autoShow: true,
+    saveWindowState: false,
+    defaultTop: 300,
+    defaultLeft: 400,
+    defaultHeight: 200,
+    defaultWidth: 200,
+    url: 'http://localhost:1337/demo/frameless-window.html',
+    frame: false
+}];
+
+test.before(async t => {
+    fin = await getConnection();
+});
+test.beforeEach(async t => {
+    for (let i = 0; i < 2; i++) {
+        windows[i] = await createChildWindow(windowOptions[i]);  
+    }
+});
+test.afterEach.always(async t => {
+    for (let i = 0; i < windows.length; i++) {
+        await windows[i].close();
+    }
+    windows = new Array<Window>(2);
+
+    // Re-enable docking after each test as service state persists through whole run
+    await disableDocking(false);
+});
+
+test('docking enabled - normal behaviour expected', async t => {
+    let bounds: NormalizedBounds[];
+
+    await dragSideToSide(windows[1], 'left', windows[0], 'right');
+
+    await assertGrouped(windows[0], windows[1], t);
+    bounds = await promiseMap(windows, win => getBounds(win));
+    t.is(bounds[0].right, bounds[1].left);
+
+    await dragWindowTo(windows[0], 200,400);
+
+    await assertGrouped(windows[0], windows[1], t);
+    bounds = await promiseMap(windows, win => getBounds(win));
+    t.is(bounds[0].right, bounds[1].left);
+
+});
+
+test('docking disabled - windows should snap but not dock', async t => {
+    await disableDocking(true);
+    
+    let bounds: NormalizedBounds[];
+
+    await dragSideToSide(windows[1], 'left', windows[0], 'right');
+
+    await assertNotGrouped(windows[0], t);
+    await assertNotGrouped(windows[1],t );
+    bounds = await promiseMap(windows, win => getBounds(win));
+    t.is(bounds[0].right, bounds[1].left);
+
+    await dragWindowTo(windows[0], 400,400);
+
+    await assertNotGrouped(windows[0], t);
+    await assertNotGrouped(windows[1],t );
+    bounds = await promiseMap(windows, win => getBounds(win));
+    t.not(bounds[0].right, bounds[1].left);
+
+    
+});
+
+async function disableDocking(disabled: boolean): Promise<void> {
+    fin.InterApplicationBus.send({uuid: 'layouts-service', name: 'layouts-service'}, 'layoutsService:experimental:disableDocking', disabled);
+
+    // hard code a tiny delay here as IAB is fire-and-forget
+    return delay(100);
+}

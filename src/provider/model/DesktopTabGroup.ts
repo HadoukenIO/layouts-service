6674a859fa5639d060bbaee4344eab42ae2f9d66
tomrobertsOF <38854624+tomrobertsOF@users.noreply.github.com>
2018-10-29 14:52:33 +0000

    [SERVICE-271] Make S&R placeholder tab titles more user-friendly (#179)
    * Changes the title of Placeholder Windows in a Tabset to display `Loading...` instead of the randomized placeholder title.

--- src/provider/model/DesktopTabGroup.ts
+++ src/provider/model/DesktopTabGroup.ts
@@ -453,7 +453,14 @@ export class DesktopTabGroup {
 
     private getTabProperties(tab: DesktopWindow): TabProperties {
         const savedProperties: string|null = localStorage.getItem(tab.getId());
-        return savedProperties ? JSON.parse(savedProperties) : {icon: tab.getState().icon, title: tab.getState().title};
+        if (savedProperties) {
+            return JSON.parse(savedProperties);
+        }
+
+        const {icon, title} = tab.getState();
+        // Special handling for S&R placeholder windows
+        const modifiedTitle = tab.getIdentity().uuid === fin.Window.me.uuid && title.startsWith('Placeholder-') ? 'Loading...' : title;
+        return {icon, title: modifiedTitle};
     }
 
     private onWindowTeardown(window: DesktopWindow): void {

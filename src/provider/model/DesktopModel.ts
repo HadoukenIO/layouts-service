390a5f907788f423a60365139c20e0b4348764b5
Tom Roberts <troberts@openfin.co>
2018-11-05 12:34:58 +0000

    Ignore error message windows in registerWindow

--- src/provider/model/DesktopModel.ts
+++ src/provider/model/DesktopModel.ts
@@ -39,7 +39,13 @@ export class DesktopModel {
         const serviceUUID: string = fin.Application.me.uuid;
 
         // Listen for any new windows created and register them with the service
-        fin.System.addListener('window-created', (evt: WindowEvent<'system', 'window-created'>) => {
+        fin.System.addListener('window-created', (evt: WindowEvent<'system', 'window-created'>) => {        
+            // Filter out error windows (which should never register)
+            if (this.isErrorWindow(evt.uuid)) {
+                console.log('Ignoring error window: ' + evt.uuid);
+                return;
+            }
+
             if (evt.uuid !== serviceUUID || evt.name.indexOf('Placeholder-') === 0) {
                 this.registerWindow({name: evt.name, uuid: evt.uuid});
             }
@@ -47,8 +53,8 @@ export class DesktopModel {
 
         fin.System.getAllWindows().then(apps => {
             apps.forEach((app) => {
-                // Ignore the main service window and all of it's children
-                if (app.uuid !== serviceUUID) {
+                // Ignore openfin error windows, the main service window, and all of it's children
+                if (this.isErrorWindow(app.uuid) && app.uuid !== serviceUUID) {
                     // Register the main window
                     this.registerWindow({uuid: app.uuid, name: app.mainWindow.name});
 
@@ -265,5 +271,9 @@ export class DesktopModel {
             }
             this.snapGroups.splice(index, 1);
         }
+    }
+
+    private isErrorWindow(uuid: string): boolean {
+        return /error-app-[a-z0-9]{8}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{12}/g.test(uuid);
     }
 }

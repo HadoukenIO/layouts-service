54cf53d28245bc54283c7b08d2cf0230ecb33e19
Tom Roberts <troberts@openfin.co>
2018-11-13 11:10:08 +0000

    slight improvements to zip script

--- scripts/createProviderZip.js
+++ scripts/createProviderZip.js
@@ -5,6 +5,12 @@ var archiver = require('archiver');
 var output = fs.createWriteStream(path.resolve(__dirname,'..','dist','provider','layouts-service.zip'));
 var archive = archiver('zip', {zlib: {level: 9}});
 
+output.on('close', () => {
+    console.log(`Zip file created at 'dist/provider/layouts-service.zip`);
+    console.log(`${archive.pointer()} total bytes written`);
+})
+
+// Docs recommend explicitly handling this response (https://archiverjs.com/docs/)
 archive.on('warning', function (err) {
     if (err.code === 'ENOENT') {
         // log warning
@@ -14,6 +20,7 @@ archive.on('warning', function (err) {
     }
 });
 
+// Docs recommend explicitly handling this response
 archive.on('error', function (err) {
     throw err;
 });

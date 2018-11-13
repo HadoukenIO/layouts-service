2ade02b8494af12977335608320c04b4ed538b52
Tom Roberts <troberts@openfin.co>
2018-11-13 11:21:42 +0000

    Exclude docs from zip

--- scripts/createProviderZip.js
+++ scripts/createProviderZip.js
@@ -10,28 +10,11 @@ output.on('close', () => {
     console.log(`${archive.pointer()} total bytes written`);
 })
 
-// Docs recommend explicitly handling this response (https://archiverjs.com/docs/)
-archive.on('warning', function (err) {
-    if (err.code === 'ENOENT') {
-        // log warning
-    } else {
-        // throw error
-        throw err;
-    }
-});
-
-// Docs recommend explicitly handling this response
-archive.on('error', function (err) {
-    throw err;
-});
-
 archive.pipe(output);
 
 // Include all provider res files except app.json (which is also in dist)
 archive.glob('**/*.!(json)', {cwd: path.resolve(__dirname, '..', 'res', 'provider')});
 // Include all provider dist files except the zip itself
 archive.glob('**/*.!(zip)', {cwd: path.resolve(__dirname, '..', 'dist', 'provider')});
-// Include the docs
-archive.directory('dist/docs/', 'docs')
 
 archive.finalize();

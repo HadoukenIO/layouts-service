a1991d9e99c09fcc40571970e7ea47492f180961
Tom Roberts <troberts@openfin.co>
2018-11-27 16:08:41 +0000

    Harden tests and move layoutsClient import to utils

--- test/demo/tabbing/tabGroupResizeConstraints.test.ts
+++ test/demo/tabbing/tabGroupResizeConstraints.test.ts
@@ -1,50 +1,54 @@
-import {test} from 'ava';
-import {Fin} from 'hadouken-js-adapter';
+import {test, TestContext} from 'ava';
+import {_Window} from 'hadouken-js-adapter/out/types/src/api/window/window';
 
 import {assertAllTabbed} from '../../provider/utils/assertions';
-import {getConnection} from '../../provider/utils/connect';
 import {delay} from '../../provider/utils/delay';
+import {Side, sideArray} from '../../provider/utils/SideUtils';
 import {Constraints} from '../snapanddock/resizeOnSnap.test';
 import {CreateWindowData, createWindowTest} from '../utils/createWindowTest';
 import {refreshWindowState} from '../utils/modelUtils';
 import {testParameterized} from '../utils/parameterizedTestUtils';
+import {layoutsClientPromise} from '../utils/serviceUtils';
 
 interface TabConstraintsOptions extends CreateWindowData {
     windowConstraints: Constraints[];
 }
 
-// Some questionable code that allows using the layouts client directly in tests
-let layoutsClient: typeof import('../../../src/client/main');
-test.before(async () => {
-    layoutsClient = await getConnection().then(fin => {
-        (global as NodeJS.Global & {fin: Fin}).fin = fin;
-        (global as NodeJS.Global & {PACKAGE_VERSION: string}).PACKAGE_VERSION = 'TEST-CLIENT';
-        return import('../../../src/client/main');
-    });
-});
-
 testParameterized(
     'Constraints applied to all tabs in group',
     [
-        {frame: true, windowCount: 2, windowConstraints: [{resizable: false}]},
-        {frame: true, windowCount: 2, windowConstraints: [{maxHeight: 500, minWidth: 100}]},
+        {frame: true, windowCount: 2, windowConstraints: [{resizable: false}, {}]},
+        {frame: true, windowCount: 2, windowConstraints: [{maxHeight: 500, minWidth: 100}, {}]},
         {
             frame: true,
             windowCount: 3,
             windowConstraints: [
                 {resizeRegion: {sides: {top: false, left: false, bottom: true, right: true}}},
-                {resizeRegion: {sides: {top: true, left: true, bottom: false, right: false}}}
+                {resizeRegion: {sides: {top: true, left: true, bottom: false, right: false}}},
+                {}
+            ]
+        },
+        {
+            frame: true,
+            windowCount: 3,
+            windowConstraints: [
+                {resizeRegion: {sides: {top: false, left: false, bottom: true, right: true}}},
+                {resizable: false},
+                {}
             ]
         },
     ],
     createWindowTest(async (t, options: TabConstraintsOptions) => {
+        const layoutsClient = await layoutsClientPromise;
+
         const windows = t.context.windows;
 
         await Promise.all(windows.map((win, index) => win.updateOptions(options.windowConstraints[index])));
         await Promise.all(windows.map((win) => refreshWindowState(win.identity)));
 
-        await refreshWindowState(windows[0].identity);
+        const startingState = await Promise.all(windows.map(win => getNormalizedConstraints(win)));
 
+        // Tab the windows
         await layoutsClient.createTabGroup(windows.map(win => win.identity));
 
         await delay(1000);
@@ -53,60 +57,85 @@ testParameterized(
 
         const resultingConstraints = constraintsUnion(...options.windowConstraints);
 
+        // Check constraints applied to all tabs
         for (let i = 0; i < windows.length; i++) {
-            const windowOptions = await windows[i].getOptions().then(options => {
-                if (options.maxHeight < 0) {
-                    options.maxHeight = Number.MAX_SAFE_INTEGER;
-                }
-                if (options.maxWidth < 0) {
-                    options.maxWidth = Number.MAX_SAFE_INTEGER;
-                }
-                return options;
-            });
+            const windowOptions: Required<Constraints> = await getNormalizedConstraints(windows[i]);
+            assertConstraintsMatch(resultingConstraints, windowOptions, t);
+        }
 
-            for (const key of Object.keys(resultingConstraints) as (keyof typeof resultingConstraints)[]) {
-                if (resultingConstraints.hasOwnProperty(key)) {
-                    if (typeof resultingConstraints[key] === 'object') {
-                        t.deepEqual(
-                            windowOptions[key],
-                            resultingConstraints[key],
-                            `${key} does not match for window ${i} (name: ${windows[i].identity.name}). Expected: ${
-                                JSON.stringify(resultingConstraints[key])}. Received: ${JSON.stringify(windowOptions[key])}`);
-                    } else {
-                        t.is(
-                            windowOptions[key],
-                            resultingConstraints[key],
-                            `${key} does not match. Expected: ${resultingConstraints[key]}. Received ${windowOptions[key]}`);
-                    }
-                }
-            }
+        // Untab the windows
+        for (let i = 1; i < windows.length; i++) {
+            await layoutsClient.removeTab(windows[i].identity);
+        }
+
+        await delay(200);
+
+        // Check constraints correctly reverted to original values
+        for (let i = 0; i < windows.length; i++) {
+            const finalState = await getNormalizedConstraints(windows[i]);
+
+            assertConstraintsMatch(startingState[i], finalState, t);
         }
     }));
 
-function constraintsUnion(...windowConstraints: Constraints[]): Constraints {
-    const result: Constraints = {};
+const defaultConstraints: Required<Constraints> = {
+    maxHeight: -1,
+    maxWidth: -1,
+    minHeight: 0,
+    minWidth: 0,
+    resizable: true,
+    resizeRegion: {
+        sides: {
+            top: true,
+            bottom: true,
+            left: true,
+            right: true,
+        }
+    }
+};
 
-    for (const constraint of windowConstraints) {
-        result.minWidth = Math.max(result.minWidth || 0, constraint.minWidth || 0);
-        result.maxWidth = Math.min(
-            result.maxWidth || Number.MAX_SAFE_INTEGER, (!constraint.maxWidth || constraint.maxWidth < 0) ? Number.MAX_SAFE_INTEGER : constraint.maxWidth);
-        result.minHeight = Math.max(result.minHeight || 0, constraint.minHeight || 0);
-        result.maxHeight = Math.min(
-            result.maxHeight || Number.MAX_SAFE_INTEGER, (!constraint.maxHeight || constraint.maxHeight < 0) ? Number.MAX_SAFE_INTEGER : constraint.maxHeight);
-
-        let resizableSide = !constraint.resizeRegion;
-        if (constraint.resizeRegion) {
-            if (!result.resizeRegion) {
-                result.resizeRegion = {sides: {top: true, bottom: true, left: true, right: true}};
-            }
-            for (const key of Object.keys(constraint.resizeRegion.sides) as ('top' | 'bottom' | 'left' | 'right')[]) {
-                result.resizeRegion.sides[key] = result.resizeRegion.sides[key] && constraint.resizeRegion.sides[key];
-                resizableSide = resizableSide || result.resizeRegion.sides[key];
+function assertConstraintsMatch(expected: Constraints, actual: Constraints, t: TestContext): void {
+    for (const key of Object.keys(defaultConstraints) as (keyof Constraints)[]) {
+        if (actual.hasOwnProperty(key) && expected.hasOwnProperty(key)) {
+            if (typeof actual[key] === 'object') {
+                t.deepEqual(
+                    expected[key], actual[key], `${key} does not match. Expected: ${JSON.stringify(expected[key])}. Received: ${JSON.stringify(actual[key])}`);
+            } else {
+                t.is(expected[key], actual[key], `${key} does not match. Expected: ${expected[key]}. Received ${actual[key]}`);
             }
         }
+    }
+}
 
+async function getNormalizedConstraints(win: _Window): Promise<Required<Constraints>> {
+    return win.getOptions().then((options: Required<Constraints>) => {
+        return constraintsUnion(options);
+    });
+}
 
-        result.resizable = !(result.resizable === false) && !(constraint.resizable === false) && resizableSide;
+function constraintsUnion(...windowConstraints: Constraints[]): Required<Constraints> {
+    const result: Required<Constraints> = {...defaultConstraints};
+
+    result.minWidth = Math.max(...windowConstraints.map(constraint => constraint.minWidth || 0));
+    result.minHeight = Math.max(...windowConstraints.map(constraint => constraint.minHeight || 0));
+    result.maxWidth =
+        Math.min(...windowConstraints.map(constraint => (!constraint.maxWidth || constraint.maxWidth < 0) ? Number.MAX_SAFE_INTEGER : constraint.maxWidth));
+    result.maxHeight =
+        Math.min(...windowConstraints.map(constraint => (!constraint.maxHeight || constraint.maxHeight < 0) ? Number.MAX_SAFE_INTEGER : constraint.maxHeight));
+
+    result.resizable = windowConstraints.map(constraint => constraint.resizable || constraint.resizable === undefined).every(val => val);
+
+    result.resizeRegion = defaultConstraints.resizeRegion;
+    sideArray.forEach(side => {
+        result.resizeRegion.sides[side] = result.resizable &&
+            windowConstraints
+                .map(constraint => constraint.resizeRegion === undefined || constraint.resizeRegion.sides[side] || constraint.resizeRegion === undefined)
+                .every(val => val);
+    });
+
+    // Check if the union of side constraints made the window non-resizable
+    if (result.resizable) {
+        result.resizable = (Object.keys(result.resizeRegion.sides) as Side[]).every(key => result.resizeRegion.sides[key]);
     }
 
     return result;

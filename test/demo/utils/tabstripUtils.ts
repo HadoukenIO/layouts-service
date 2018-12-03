957f2c2b6d268dcb922a812f897f6f4a98fe493e
Tom Roberts <troberts@openfin.co>
2018-12-03 09:43:57 +0000

    Added documentation to tabstrip utils.

--- test/demo/utils/tabstripUtils.ts
+++ test/demo/utils/tabstripUtils.ts
@@ -10,22 +10,34 @@ import { Point } from '../../../src/provider/snapanddock/utils/PointUtils';
 
 const MAX_TAB_WIDTH = 220;  // From tabstrip CSS
 
+/**
+ * Util using RobotJS to click on the handle for a specified tab, marking it active.
+ */
 export async function switchTab(tabstrip: _Window, tabIndex: number) {
     await mouseOverTabHandle(tabstrip, tabIndex);
     await robot.mouseClick();
-    await delay(1000);
+    await delay(500);
 }
 
+/**
+ * Util using RobotJS to drag a tab handle up and out of the tabstrip, ejecting the tab.
+ */
 export async function tearoutTab(tabstrip: _Window, tabIndex: number) {
     const bounds: Bounds = await getBounds(tabstrip);
     return tearoutToPoint(tabstrip, tabIndex, {x: bounds.left - 40, y: bounds.top - 40});
 }
 
+/**
+ * Util using RobotJS to drag a tab handle over a different tabstrip, ejecting from the source and adding to the target.
+ */
 export async function tearoutToOtherTabstrip(sourceTabstrip: _Window, tabIndex: number, targetTabstrip: _Window) {
     const targetBounds: NormalizedBounds = await getBounds(targetTabstrip);
     return tearoutToPoint(sourceTabstrip, tabIndex, {x: targetBounds.left + 30, y: targetBounds.top + 30});
 }
 
+/**
+ * Util using RobotJS to drag a tab handle to a specific point, ejecting from the tabset. 
+ */
 export async function tearoutToPoint(sourceTabstrip: _Window, tabIndex: number, target: Point<number>) {
     await mouseOverTabHandle(sourceTabstrip, tabIndex);
     await robot.mouseToggle('down');
@@ -34,6 +46,9 @@ export async function tearoutToPoint(sourceTabstrip: _Window, tabIndex: number, 
     await delay(1000);
 }
 
+/**
+ * Private helper to position the mouse over the correct tab handle.
+ */
 async function mouseOverTabHandle(tabstrip: _Window, tabIndex: number) {
     const tabs = await getTabbedWindows(tabstrip.identity);
 

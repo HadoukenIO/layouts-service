870a6bc18bf8ad3e98063f164605865c2610f6e4
Michael Coates <MichaelMCoates@gmail.com>
2018-12-03 13:41:33 -0500

    Remove Commented code

--- test/demo/utils/workspacesUtils.ts
+++ test/demo/utils/workspacesUtils.ts
@@ -8,43 +8,6 @@ import {sendServiceMessage} from './serviceUtils';
 
 type SaveRestoreTestContext = GenericTestContext<Context<AppContext>>;
 
-
-// export async function createApp(t: SaveRestoreTestContext, uuid: string, registration: string, top?: number, left?: number) {
-//     const registered = registration === 'registered';
-//     const fin = await getConnection();
-//     const app = await fin.Application.create({
-//         uuid,
-//         url: registered ? 'http://localhost:1337/test/registeredApp.html' : 'http://localhost:1337/test/deregisteredApp.html',
-//         name: uuid,
-//         mainWindowOptions: {autoShow: true, saveWindowState: false, defaultTop: top || 200, defaultLeft: left || 200, defaultHeight: 300, defaultWidth: 300}
-//     });
-//     t.context.apps.push(app);
-//     await app.run();
-//     if (!registered) {
-//         // Delay to allow the window to deregister from the service.
-//         await delay(500);
-//     }
-//     return app;
-// }
-
-// export async function createCloseAndRestoreLayoutOLD(t: SaveRestoreTestContext, windowCreationFunction: PassFunction, pass: boolean) {
-//     const fin = await getConnection();
-//     const generatedLayout = await sendServiceMessage('generateLayout', undefined);
-//     await Promise.all(t.context.apps.map((app: Application) => app.close()));
-//     await fin.System.addListener('window-created', windowCreationFunction);
-//     await sendServiceMessage('restoreLayout', generatedLayout);
-//     setTimeout(() => {
-//         if (!pass) {
-//             t.context.y();
-//             t.pass();
-//         } else {
-//             t.context.n('Too long');
-//             t.fail();
-//         }
-//     }, 3000);
-//     await t.context.p;
-// }
-
 async function isWindowActive(uuid: string, name: string) {
     const fin = await getConnection();
     const allWindows = await fin.System.getAllWindows();

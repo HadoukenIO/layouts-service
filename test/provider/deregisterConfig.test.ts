53d4a7cfe45818c20a0a9ec49060f8c87e4c446c
Philip Broadbent <philip@openfin.co>
2019-01-21 14:43:28 +0000

    [SERVICE-306] Added additional test to cover deregistering a window in a snapped tabgroup.

--- test/provider/deregisterConfig.test.ts
+++ test/provider/deregisterConfig.test.ts
@@ -9,7 +9,7 @@ import {isWindowRegistered} from '../demo/utils/snapServiceUtils';
 import {assertGrouped, assertTabbed} from './utils/assertions';
 import {createChildWindow} from './utils/createChildWindow';
 import {delay} from './utils/delay';
-import {dragSideToSide} from './utils/dragWindowTo';
+import {dragSideToSide, dragWindowTo} from './utils/dragWindowTo';
 import {tabWindowsTogether} from './utils/tabWindowsTogether';
 
 type TestContext = GenericTestContext<Context<{windows: Window[]}>>;
@@ -97,4 +97,39 @@ test('When a tabbed window is de-registered, it is removed from its tab group', 
     const groups = await Promise.all(windows.map(w => w.getGroup()));
     t.is(groups[0].length, 0);
     t.is(groups[1].length, 0);
+});
+
+test('When a tabbed window is de-registered, it is removed from its snapped tab group', async (t: TestContext) => {
+    const windows = t.context.windows;
+    for (let i = 0; i < 4; i++) {
+        windows.push(
+            await createChildWindow({...DEFAULT_OPTIONS, name: `testWindow${i + 1}`, defaultLeft: (i % 2) * 320, defaultTop: Math.floor(i / 2) * 220}));
+    }
+
+    // Setup tab/snap groups.
+    // Windows are frameless, so can snap by windows rather than by tabstrips
+    await tabWindowsTogether(windows[0], windows[1]);
+    await tabWindowsTogether(windows[2], windows[3]);
+    await dragSideToSide(windows[0], 'left', windows[2], 'right');
+    const bounds = await windows[0].getBounds();
+    await dragWindowTo(windows[0], bounds.left + 100, bounds.top + 100);
+
+    // Ensure windows are in position
+    await assertTabbed(windows[0], windows[1], t);
+    await assertTabbed(windows[2], windows[3], t);
+    await assertGrouped(t, ...windows);
+
+    // De-register first window
+    await addRuleToProvider({level: 'window', uuid: 'testApp', name: 'testWindow1'}, {enabled: false});
+
+    // Ensure first window is de-registered
+    t.false(await isWindowRegistered(windows[0].identity));
+    for (let i = 1; i < 4; i++) {
+        t.true(await isWindowRegistered(windows[i].identity));
+    }
+
+    // Ensure first window is de-tabbed whilst others remain grouped
+    const groups = await Promise.all(windows.map(w => w.getGroup()));
+    const groupSizes = groups.map(g => g.length);
+    t.deepEqual(groupSizes, [0, 4, 4, 4]);  // 4 Windows in group - 3 tabs and the one remaining tabstrip
 });

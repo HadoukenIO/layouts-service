387b79bd2a5a7756b781e1a090054ccc36076263
ryanpetersonOF <ryan@openfin.co>
2019-02-01 15:40:12 +0000

    Update testApp.js PR

--- res/test/testApp.js
+++ res/test/testApp.js
@@ -1,11 +1,11 @@
 const main = async () => {
-    fin.desktop.InterApplicationBus.Channel.connect('of-layouts-service-v1').then(client => {
-        client.dispatch('DEREGISTER');
+    fin.InterApplicationBus.Channel.connect('of-layouts-service-v1').then(client => {
+        client.dispatch('DEREGISTER', fin.Window.me);
         client.register('SET-SAVE-HANDLER', () => {});
         client.register('SET-RESTORE-HANDLER', (a) => a);
     });
     
-    let provider = await fin.desktop.InterApplicationBus.Channel.create('test-app-comms');
+    let provider = await fin.InterApplicationBus.Channel.create('test-app-comms');
 
     provider.register('createWindow', (payload) => new Promise((res, rej) => {
         new fin.desktop.Window(payload, () => res(), rej);

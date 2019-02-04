fe3520c89a08b0e9985fa1748456ae9693717e68
Nicholas Goodman <nicholas@openfin.co>
2019-02-04 07:24:41 -0500

    SERVICE-338 Initial commit of .NET client implementation.

---
+++ cs-client/OpenFin.Layouts/LayoutManager.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Fin = Openfin.Desktop;
+
+namespace OpenFin.Layouts
+{
+    public class WindowLayoutManager
+    {
+        private const string LayoutsServiceChannelName = "of-layouts-service-v1";
+        private const string LayoutsHostAppUuid = "of-layouts-dotnet-host";
+        private const string LayoutsHelperAppUrl = "about:blank";
+
+        private const string LayoutsServiceManifestUrl = "https://cdn.openfin.co/services/openfin/layouts/app.json";
+
+        private static Fin.Runtime RuntimeInstance;
+        private static Fin.ChannelClient ChannelClient;
+
+        private string _observedWindowName;
+        private Fin.ExternalWindowObserver _observer;
+
+        public WindowLayoutManager(IntPtr handle)
+        {
+            _observedWindowName = Guid.NewGuid().ToString();
+            _observer = new Fin.ExternalWindowObserver(RuntimeInstance, LayoutsHostAppUuid, _observedWindowName, handle);
+        }
+
+        public static void Initialize()
+        {
+            Initialize(new Uri(LayoutsServiceManifestUrl));
+        }
+
+        public static void Initialize(Uri manifestUri)
+        {
+            var runtimeOptions = Fin.RuntimeOptions.LoadManifest(manifestUri);
+
+            RuntimeInstance = Fin.Runtime.GetRuntimeInstance(runtimeOptions);
+            RuntimeInstance.Connect(() => 
+            {
+                var layoutsService = RuntimeInstance.CreateApplication(runtimeOptions.StartupApplicationOptions);
+
+                layoutsService.isRunning(ack =>
+                {
+                    if (!(bool)(ack.getData() as Newtonsoft.Json.Linq.JValue).Value)
+                    {
+                        layoutsService.run();
+                    }
+
+                    ChannelClient = RuntimeInstance.InterApplicationBus.Channel.CreateClient(LayoutsServiceChannelName);
+                    ChannelClient.Connect();
+                });
+            });
+        }
+
+        public void Register()
+        {
+            RuntimeInstance.Connect(() =>
+            {
+                var appOptions = new Fin.ApplicationOptions(LayoutsHostAppUuid, LayoutsHostAppUuid, LayoutsHelperAppUrl);
+                var layoutsHostApp = RuntimeInstance.CreateApplication(appOptions);
+
+                layoutsHostApp.isRunning(ack =>
+                {
+                    if (!(bool)(ack.getData() as Newtonsoft.Json.Linq.JValue).Value)
+                    {
+                        layoutsHostApp.run();
+                    }
+
+                    _observer.onReady();
+                });
+            });
+        }
+
+        public void Ungroup()
+        {
+            ChannelClient.Dispatch<object>("undockWindow", new
+            {
+                uuid = LayoutsHostAppUuid,
+                name = _observedWindowName
+            });
+        }
+
+        public void UngroupAll()
+        {
+            ChannelClient.Dispatch<object>("undockGroup", new
+            {
+                uuid = LayoutsHostAppUuid,
+                name = _observedWindowName
+            });
+        }
+    }
+}
---
+++ cs-client/test.wpf/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Interop;
+using Fin = Openfin.Desktop;
+using Layouts = OpenFin.Layouts;
+
+namespace test.wpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Layouts.WindowLayoutManager _layoutManager;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Layouts.WindowLayoutManager.Initialize();
+
+            var helper = new WindowInteropHelper(this);
+            helper.EnsureHandle();
+
+            _layoutManager = new Layouts.WindowLayoutManager(helper.Handle);
+            _layoutManager.Register();
+        }
+
+        private void UndockButton_Click(object sender, RoutedEventArgs e)
+        {
+            _layoutManager.Ungroup();   
+        }
+
+        private void UndockGroup_Click(object sender, RoutedEventArgs e)
+        {
+            _layoutManager.UngroupAll();
+        }
+    }
+}

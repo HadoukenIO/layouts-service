96ad7466a07f4e8cc32871df319f4ce0a5ece065
ryanpetersonOF <ryan@openfin.co>
2019-02-18 10:56:49 +0000

    More Linting

--- src/provider/ServiceWorker.js
+++ src/provider/ServiceWorker.js
@@ -1,51 +1,46 @@
- // Set a name for the current cache
-const cacheName = PACKAGE_VERSION; 
+// Set a name for the current cache
+const cacheName = PACKAGE_VERSION;
 
 // Default files to always cache
 const cacheFiles = [
-	'./provider.html',
-	'./main-bundle.js',
-	'./tabStrip-bundle.js',
-	'./tabbing/tabstrip/tabstrip.html',
-	'./tabbing/tabstrip/css/tabs.css',
-	'./tabbing/tabstrip/css/image/close.png',
-	'./tabbing/tabstrip/css/image/maximise.png',
-	'./tabbing/tabstrip/css/image/minimize.png',
-	'./tabbing/tabstrip/css/image/restore.png'
-]
+    './provider.html',
+    './main-bundle.js',
+    './tabStrip-bundle.js',
+    './tabbing/tabstrip/tabstrip.html',
+    './tabbing/tabstrip/css/tabs.css',
+    './tabbing/tabstrip/css/image/close.png',
+    './tabbing/tabstrip/css/image/maximise.png',
+    './tabbing/tabstrip/css/image/minimize.png',
+    './tabbing/tabstrip/css/image/restore.png'
+];
 
-self.addEventListener('install', function(e) {
+self.addEventListener('install', (e) => {
     // e.waitUntil Delays the event until the Promise is resolved
     e.waitUntil(
         // Open the cache
-        caches.open(cacheName).then(function(cache) {
-
-        // Add all the default files to the cache
-        return cache.addAll(cacheFiles);
-    })
-	);
-});
-
-self.addEventListener('activate', function(e) {
-    e.waitUntil(
-    	// Get all the cache keys (cacheName)
-    caches.keys().then(function(cacheNames) {
-        return Promise.all(cacheNames.map(function(thisCacheName) {
-
-            // If a cached item is saved under a previous cacheName
-            if (thisCacheName !== cacheName) {
-                // Delete that cached file
-                return caches.delete(thisCacheName);
-            }
+        caches.open(cacheName).then((cache) => {
+            // Add all the default files to the cache
+            return cache.addAll(cacheFiles);
         }));
-		 })
-	 );
 });
 
-self.addEventListener('fetch', function(event) {
+self.addEventListener('activate', (e) => {
+    e.waitUntil(
+        // Get all the cache keys (cacheName)
+        caches.keys().then((cacheNames) => {
+            return Promise.all(cacheNames.map((thisCacheName) => {
+                // If a cached item is saved under a previous cacheName
+                if (thisCacheName !== cacheName) {
+                    // Delete that cached file
+                    return caches.delete(thisCacheName);
+                }
+            }));
+        }));
+});
+
+self.addEventListener('fetch', (event) => {
     event.respondWith(
-        fetch(event.request).catch(function() {
-           return caches.match(event.request);
-        })
-    );
+        fetch(event.request).catch(() => {
+            return caches.match(event.request);
+        }));
 });

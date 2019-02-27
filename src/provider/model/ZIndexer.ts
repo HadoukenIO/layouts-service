f92232fc350c8f8bb6d931e09be13d1586424571
bryangaleOF <47325366+bryangaleOF@users.noreply.github.com>
2019-02-27 12:34:53 +0000

    [SERVICE-357-fix] Update ZIndexer to track 'active' state of windows  (#310)

--- src/provider/model/ZIndexer.ts
+++ src/provider/model/ZIndexer.ts
@@ -16,6 +16,7 @@ export interface ZIndex {
     id: string;
     identity: WindowIdentity;
     bounds: Rect;
+    active: boolean;
 }
 
 interface ObjectWithIdentity {
@@ -64,14 +65,6 @@ export class ZIndexer {
     }
 
     /**
-     * Returns the array of indexes.
-     * @returns {ZIndex[]} ZIndex[]
-     */
-    public get indexes(): ReadonlyArray<ZIndex> {
-        return this._stack;
-    }
-
-    /**
      * Takes a list of window-like items and returns the top-most item from the list.
      *
      * NOTE: Implementation will not return any item within the input that does not exist within the ZIndexer util.
@@ -83,23 +76,28 @@ export class ZIndexer {
         const ids: string[] = this.getIds(items);
 
         for (const item of this._stack) {
-            const index: number = ids.indexOf(item.id);
-            if (index >= 0) {
-                return items[index];
+            if (item.active) {
+                const index: number = ids.indexOf(item.id);
+                if (index >= 0) {
+                    return items[index];
+                }
             }
         }
 
         return null;
     }
 
-    public getWindowAt(x: number, y: number, exclude?: WindowIdentity): WindowIdentity|null {
+    public getWindowAt(x: number, y: number, exclusions: WindowIdentity[]): WindowIdentity|null {
         const entry: ZIndex|undefined = this._stack.find((item: ZIndex) => {
             const identity = item.identity;
 
-            if (identity.uuid === SERVICE_IDENTITY.uuid && !identity.name.startsWith('TABSET-')) {
+            if (!item.active) {
+                // Exclude inactive windows
+                return false;
+            } else if (identity.uuid === SERVICE_IDENTITY.uuid && !identity.name.startsWith('TABSET-')) {
                 // Exclude service-owned windows
                 return false;
-            } else if (exclude && exclude.uuid === identity.uuid && exclude.name === identity.name) {
+            } else if (exclusions.some(exclusion => exclusion.uuid === identity.uuid && exclusion.name === identity.name)) {
                 // Item is excluded
                 return false;
             } else {
@@ -113,78 +111,59 @@ export class ZIndexer {
     }
 
     /**
-     * Takes a list of window-like items, and sorts them by the z-index of their respective windows.
-     *
-     * NOTE: Implementation will filter-out any windows within the input that do not exist within the ZIndexer util.
-     *
-     * @param items
-     */
-    public sort<T extends Identifiable>(items: T[]): T[] {
-        const ids: string[] = this.getIds(items);
-        const result: T[] = [];
-
-        for (const item of this._stack) {
-            const index: number = ids.indexOf(item.id);
-            if (index >= 0) {
-                result.push(items[index]);
-            }
-        }
-
-        return result;
-    }
-
-    /**
-     * Updates the windows index in the stack and sorts array.
+     * Updates the state of the window tracked in the stack, and resorts windows
      * @param identity ID of the window to update (uuid, name)
-     * @param bounds Physical bounds of the window to update, if known
+     * @param active The active state of the window to update, if known. Will guess as true if needed and not specified
+     * @param bounds Physical bounds of the window to update, if known. Will perform an async query if needed and not specified
+     * @param timestamp The new timestamp for the stack entry. Will use the current time if not specified
      */
-    private update(identity: WindowIdentity, bounds?: Rect) {
+    private update(identity: WindowIdentity, active?: boolean, bounds?: Rect, timestamp?: number) {
+        timestamp = timestamp !== undefined ? timestamp : Date.now();
+
         const id: string = this._model.getId(identity);
-        const timestamp = Date.now();
-        let entry: ZIndex|undefined = this._stack.find(i => i.id === id);
+        const entry: ZIndex|undefined = this._stack.find(i => i.id === id);
 
         if (entry) {
             // Update existing entry
             entry.timestamp = timestamp;
+
+            if (active !== undefined) {
+                entry.active = active;
+            }
             if (bounds) {
-                // Update bounds
                 Object.assign(entry.bounds, bounds);
             }
-        } else if (bounds) {
-            // Can create & add a new entry synchronously
-            this.addToStack({id, identity, timestamp, bounds});
-        } else {
+        } else if (!bounds) {
             // Must request bounds before being able to add
             fin.Window.wrapSync(identity).getBounds().then(bounds => {
-                // Since this required an async operation, entry may now exist within stack
-                entry = this._stack.find(i => i.id === id);
-
-                if (entry) {
-                    // Update bounds on existing entry
-                    Object.assign(entry.bounds, bounds);
-                } else {
-                    // Continue with creating new entry
-                    this.addToStack({id, identity, timestamp, bounds: this.sanitizeBounds(bounds)});
-                }
+                // Since this required an async operation, entry may now exist within stack, so recursively call update
+                this.update(identity, active, this.sanitizeBounds(bounds), timestamp);
             });
+        } else {
+            // Can create & add a new entry synchronously
+            this.addToStack({id, identity, timestamp, bounds, active: active !== undefined ? active : true});
         }
 
-        this._stack.sort((a, b) => {
-            return b.timestamp - a.timestamp;
-        });
+        this.sortStack();
     }
 
-    private onWindowModified(identity: WindowIdentity, bounds?: Rect): void {
+    private onWindowModified(identity: WindowIdentity, active?: boolean, bounds?: Rect): void {
         const modelWindow: DesktopWindow|null = this._model.getWindow(identity);
         const modelGroup: DesktopSnapGroup|null = modelWindow && modelWindow.snapGroup;
 
         if (modelGroup && modelGroup.length > 1) {
-            // Also bring-to-front any windows within the group
+            // Also modify any windows within the group
             const id: string = this._model.getId(identity);
-            modelGroup.windows.forEach(window => this.update(window.identity, window.id === id ? bounds : undefined));
+
+            modelGroup.windows.forEach(window => {
+                const windowBounds = window.id === id ? bounds : undefined;
+                const windowActive = window.id === id ? active : undefined;
+
+                this.update(window.identity, windowActive, windowBounds);
+            });
         } else {
-            // Just bring modified window to front
-            this.update(identity, bounds);
+            // Just modify single window
+            this.update(identity, active, bounds);
         }
     }
 
@@ -196,15 +175,21 @@ export class ZIndexer {
         const identity = win.identity as WindowIdentity;  // A window identity will always have a name, so it is safe to cast
 
         const bringToFront = () => {
-            this.onWindowModified(identity);
+            this.onWindowModified(identity, true);
         };
         const boundsChanged = (evt: WindowBoundsChange<'window', 'bounds-changed'>) => {
-            this.onWindowModified(identity, this.sanitizeBounds(evt));
+            this.onWindowModified(identity, undefined, this.sanitizeBounds(evt));
+        };
+        const markInactive = () => {
+            this.onWindowModified(identity, false, undefined);
         };
         const onClose = () => {
             win.removeListener('focused', bringToFront);
             win.removeListener('shown', bringToFront);
             win.removeListener('bounds-changed', boundsChanged);
+            // TODO (SERVICE-376): Uncomment these when below commented-out addListener calls are uncommented
+            // win.removeListener('hidden', markInactive);
+            // win.removeListener('minimized', markInactive);
             win.removeListener('closed', onClose);
 
             const id = `${identity.uuid}/${identity.name}`;
@@ -219,12 +204,30 @@ export class ZIndexer {
         win.addListener('shown', bringToFront);
         win.addListener('bounds-changed', boundsChanged);
 
+        // When a window is hidden or minimized, mark it as inactive
+        // TODO (SERVICE-376): Uncomment these and above removeListener calls to allow tracking of hidden non-Model
+        // windows. Blocked on figuring out why events are not properly cleaned up on Jenkins
+        // win.addListener('hidden', markInactive);
+        // win.addListener('minimized', markInactive);
+
         // Remove listeners when the window is destroyed
         win.addListener('closed', onClose);
     }
 
     private addToStack(entry: ZIndex): void {
         this._stack.push(entry);
+    }
+
+    private sortStack(): void {
+        this._stack.sort((a, b) => {
+            if (a.active && !b.active) {
+                return -1;
+            } else if (!a.active && b.active) {
+                return 1;
+            } else {
+                return b.timestamp - a.timestamp;
+            }
+        });
     }
 
     private sanitizeBounds(input: Bounds): Rect {

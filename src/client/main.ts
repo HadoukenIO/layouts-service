d132a8250c93bcd102e823bed18e17cd7a50317b
Tom Roberts <troberts@openfin.co>
2019-02-28 09:56:12 +0000

    Remove redundant type gaurds from deregister

--- src/client/main.ts
+++ src/client/main.ts
@@ -98,9 +98,6 @@ export type WindowState = 'normal'|'minimized'|'maximized';
  * @param identity The window (or pattern of windows) to deregister, defaults to the current window
  */
 export async function deregister(identity: IdentityRule = getId() as IdentityRule): Promise<void> {
-    if (!identity.uuid || !identity.name) {
-        throw new Error('Invalid window identity provided');
-    }
     return tryServiceDispatch<IdentityRule, void>(RegisterAPI.DEREGISTER, parseIdentityRule(identity));
 }
 
@@ -130,8 +127,5 @@ export async function deregister(identity: IdentityRule = getId() as IdentityRul
  * @param identity The window (or pattern of windows) to register, defaults to the current window
  */
 export async function register(identity: IdentityRule = getId() as IdentityRule): Promise<void> {
-    if (!identity.uuid || !identity.name) {
-        throw new Error('Invalid window identity provided');
-    }
     return tryServiceDispatch<IdentityRule, void>(RegisterAPI.REGISTER, parseIdentityRule(identity));
 }

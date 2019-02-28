57599f90c93875f5ce4f72b87348a9fae10f1569
Michael Coates <MichaelMCoates@gmail.com>
2019-02-28 11:06:40 -0500

    Change service uuid to use constant instead of string

--- src/provider/model/DesktopWindow.ts
+++ src/provider/model/DesktopWindow.ts
@@ -173,7 +173,7 @@ export class DesktopWindow implements DesktopEntity {
                 }
 
                 // Special handling for workspace placeholder windows
-                if (window.identity.uuid === 'layouts-service' && window.identity.name && window.identity.name.startsWith('Placeholder-')) {
+                if (window.identity.uuid === SERVICE_IDENTITY.uuid && window.identity.name && window.identity.name.startsWith('Placeholder-')) {
                     windowTitle = 'Loading...';
                 }
 

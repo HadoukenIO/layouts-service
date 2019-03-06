4037c43901a91a5cfc792901da3035b318fa2120
bryangaleOF <bryan@openfin.co>
2019-03-06 16:33:28 +0000

    [N/A] Retry tabbing windows if fail first time

--- test/provider/utils/dragWindowAndHover.ts
+++ test/provider/utils/dragWindowAndHover.ts
@@ -14,6 +14,13 @@ export const dragWindowAndHover = async (identityOrWindow: Win, x: number, y: nu
     robot.mouseToggle('up');
     robot.moveMouse(bounds.left + xOffset, bounds.top + yOffset);
     robot.mouseToggle('down');
+
+    if (x === bounds.left && y === bounds.top) {
+        robot.moveMouseSmooth(x + xOffset + 3, y + yOffset + 3);
+
+        console.warn('*** drag involves no movement, so jiggling');
+    }
+
     robot.moveMouseSmooth(x + xOffset, y + yOffset);
 
     await delay(500);
--- test/provider/utils/tabWindowsTogether.ts
+++ test/provider/utils/tabWindowsTogether.ts
@@ -4,21 +4,45 @@ import {getTabGroupID, getTabGroupIdentity} from '../../demo/utils/tabServiceUti
 
 import {delay} from './delay';
 import {dragWindowToOtherWindow} from './dragWindowTo';
+import { executeJavascriptOnService } from '../../demo/utils/serviceUtils';
 
-export async function tabWindowsTogether(target: _Window, windowToTab: _Window, expectSucceess = true) {
+export async function tabWindowsTogether(target: _Window, windowToTab: _Window, expectSucceess = true, retries = 5) {
     const isTargetTabbed: boolean = await getTabGroupIdentity(target.identity) !== null;
 
+    await executeJavascriptOnService(function(this: ProviderWindow) {
+        console.warn(`*** Starting drag, retries left ${retries}`);
+    });
     await dragWindowToOtherWindow(windowToTab, 'top-left', target, 'top-left', {x: 10, y: isTargetTabbed ? -20 : 10});
     await delay(500);
 
+    await executeJavascriptOnService(function(this: ProviderWindow) {
+        console.warn(`*** Ending drag`);
+    });
+
     if (expectSucceess) {
         const targetTabGroupID = await getTabGroupID(target.identity);
-        if (targetTabGroupID === null) {
-            console.warn(`Target window not tabbed following tabWindowsTogether (${target.identity.uuid}/${target.identity.name})`);
-        }
         const windowToTabTabGroupID = await getTabGroupID(windowToTab.identity);
-        if (windowToTabTabGroupID === null) {
-            console.warn(`Window to tab not tabbed following tabWindowsformerTogether (${windowToTab.identity.uuid}/${windowToTab.identity.name})`);
+
+        if (targetTabGroupID === null || targetTabGroupID === null) {
+            if (retries > 0) {
+                await delay(1000);
+                tabWindowsTogether(target, windowToTab, expectSucceess, retries - 1);
+            } else {
+                if (targetTabGroupID === null) {
+                    console.warn(`**** Target window not tabbed following tabWindowsTogether (${target.identity.uuid}/${target.identity.name})`);
+        
+                    console.trace();
+                    console.warn(`**************************`);
+                    await delay(10 * 60 * 60 * 1000);
+                }
+                if (windowToTabTabGroupID === null) {
+                    console.warn(`**** Window to tab not tabbed following tabWindowsformerTogether (${windowToTab.identity.uuid}/${windowToTab.identity.name})`);
+        
+                    console.trace();
+                    console.warn(`**************************`);
+                    await delay(10 * 60 * 60 * 1000);
+                }
+            }
         }
     }
 }

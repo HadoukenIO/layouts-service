a44c80c1b76311d85cf819882608e5614826da79
bryangaleOF <47325366+bryangaleOF@users.noreply.github.com>
2019-03-14 16:31:55 +0000

    [SERVICE-402] Add an undock button to the default tabstrip (#334)

--- src/provider/tabbing/tabstrip/main.ts
+++ src/provider/tabbing/tabstrip/main.ts
@@ -1,5 +1,6 @@
 import * as layouts from '../../../client/main';
 import {WindowIdentity} from '../../../client/main';
+import {WindowDockedEvent, WindowUndockedEvent} from '../../../client/snapanddock';
 import {TabActivatedEvent, TabAddedEvent, TabPropertiesUpdatedEvent, TabRemovedEvent} from '../../../client/tabbing';
 import {TabGroupMaximizedEvent, TabGroupRestoredEvent} from '../../../client/tabstrip';
 
@@ -9,10 +10,26 @@ let tabManager: TabManager;
 
 tabManager = new TabManager();
 
+interface TabstripElements {
+    minimizeElem: HTMLElement;
+    maximizeElem: HTMLElement;
+    closeElem: HTMLElement;
+    undockElem: HTMLElement;
+}
+
+const aquireTabstripElements = () => {
+    const minimizeElem: HTMLElement = document.getElementById('window-button-minimize')!;
+    const maximizeElem: HTMLElement = document.getElementById('window-button-maximize')!;
+    const closeElem: HTMLElement = document.getElementById('window-button-exit')!;
+    const undockElem: HTMLElement = document.getElementById('window-button-undock')!;
+
+    return {minimizeElem, maximizeElem, closeElem, undockElem};
+};
+
 /**
  * Creates event listeners for events fired from the openfin layouts service.
  */
-const createLayoutsEventListeners = () => {
+const createLayoutsEventListeners = (tabstripElements: TabstripElements) => {
     layouts.tabbing.addEventListener('tab-added', (event: TabAddedEvent) => {
         tabManager.addTab(event.identity, event.properties, event.index);
 
@@ -39,7 +56,7 @@ const createLayoutsEventListeners = () => {
         }
     });
 
-    const maximizeElem: HTMLElement = document.getElementById('window-button-maximize')!;
+    const maximizeElem = tabstripElements.maximizeElem;
 
     layouts.tabstrip.addEventListener('tab-group-maximized', (event: TabGroupMaximizedEvent) => {
         tabManager.isMaximized = true;
@@ -52,36 +69,49 @@ const createLayoutsEventListeners = () => {
             maximizeElem.classList.remove('restore');
         }
     });
+
+    const undockElem = tabstripElements.undockElem;
+
+    layouts.snapAndDock.addEventListener('window-docked', (event: WindowDockedEvent) => {
+        if (undockElem.classList.contains('hidden')) {
+            undockElem.classList.remove('hidden');
+        }
+    });
+
+    layouts.snapAndDock.addEventListener('window-undocked', (event: WindowUndockedEvent) => {
+        undockElem.classList.add('hidden');
+    });
 };
 
 /**
  * Creates Event Listeners for window controls (close, maximize, minimize, etc);
  */
-const createWindowUIListeners = () => {
-    const minimizeElem: HTMLElement = document.getElementById('window-button-minimize')!;
-    const maximizeElem: HTMLElement = document.getElementById('window-button-maximize')!;
-    const closeElem: HTMLElement = document.getElementById('window-button-exit')!;
-
+const createWindowUIListeners = (tabstripElements: TabstripElements) => {
     // Minimize Button
-    minimizeElem.onclick = () => {
-        layouts.tabbing.minimizeTabGroup(tabManager.getTabs[0].ID);
+    tabstripElements.minimizeElem.onclick = () => {
+        layouts.tabbing.minimizeTabGroup();
     };
 
     // Maximize / Restore button
-    maximizeElem.onclick = () => {
+    tabstripElements.maximizeElem.onclick = () => {
         if (!tabManager.isMaximized) {
-            layouts.tabbing.maximizeTabGroup(tabManager.getTabs[0].ID);
+            layouts.tabbing.maximizeTabGroup();
         } else {
-            layouts.tabbing.restoreTabGroup(tabManager.getTabs[0].ID);
+            layouts.tabbing.restoreTabGroup();
         }
     };
 
     // Close Button
-    closeElem.onclick = () => {
-        layouts.tabbing.closeTabGroup(tabManager.getTabs[0].ID);
+    tabstripElements.closeElem.onclick = () => {
+        layouts.tabbing.closeTabGroup();
+    };
+
+    tabstripElements.undockElem.onclick = () => {
+        layouts.snapAndDock.undockWindow();
     };
 };
 
+const tabstripElements = aquireTabstripElements();
 
-createLayoutsEventListeners();
-createWindowUIListeners();
+createLayoutsEventListeners(tabstripElements);
+createWindowUIListeners(tabstripElements);

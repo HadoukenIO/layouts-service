f94484ae15a8ebfbda8198272c2a571694a46880
bryangaleOF <bryan@openfin.co>
2019-03-26 13:36:17 +0000

    [SERVICE-210] Fix runner.js missing new test names

--- test/runner.js
+++ test/runner.js
@@ -79,7 +79,7 @@ Options:
     process.exit();
 }
 
-const fileNamesArg = testFileNames.length > 1 ? testFileNames.slice(1).map(testFileName => `${testFileName}.test.ts`).join(" ") : '';
+const fileNamesArg = testFileNames.length > 1 ? testFileNames.slice(1).map(testFileName => `${testFileName}.inttest.ts`).join(" ") : '';
 const testCommand = `jest ` +
     `--color=${color} ` +
     `--no-cache --config=jest-int.config.js --forceExit --runInBand ` +

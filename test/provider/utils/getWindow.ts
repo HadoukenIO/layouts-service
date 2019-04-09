89f743940d7ba13d82940d6a54ce7959ef995581
bryangaleOF <bryan@openfin.co>
2019-04-09 17:00:58 +0100

    [N/A] Comment oddness in getWindow

--- test/provider/utils/getWindow.ts
+++ test/provider/utils/getWindow.ts
@@ -5,10 +5,13 @@ import {fin} from '../../demo/utils/fin';
 export type Win = Window|Identity;
 
 export const getWindow = async (identityOrWindow: Win) => {
+    // We check constructor name as `instanceof Window` cannot be relied on in the Jest environment
     if ((identityOrWindow as any).constructor.name === '_Window') {  // tslint:disable-line:no-any
         return identityOrWindow as Window;
     }
     const identity = identityOrWindow as Identity;
 
+    // We extract fields from identity here as an extra guard against passing a full window into wrap,
+    // which can cause serialization problems later
     return fin.Window.wrap({uuid: identity.uuid, name: identity.name});
 };

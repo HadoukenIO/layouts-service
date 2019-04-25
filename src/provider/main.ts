c8cca9e0229e74db22712d26479d2d4267463d0b
bryangaleOF <47325366+bryangaleOF@users.noreply.github.com>
2019-04-25 18:05:04 +0100

    [N/A] Enable display scaling for 150% and 200% display scales (#385)

--- src/provider/main.ts
+++ src/provider/main.ts
@@ -35,16 +35,20 @@ declare const window: Window&{
 
 fin.desktop.main(main);
 
+const supportedScaleFactors = [1, 1.5, 2];
+
 export async function main() {
     const monitorInfo = await fin.System.getMonitorInfo();
 
-    // Disable the service if display scaling is not 100%
-    if (monitorInfo.deviceScaleFactor !== 1) {
+    // Disable the service if display scaling is not a supported scale factor
+    if (!supportedScaleFactors.some(scaleFactor => scaleFactor === monitorInfo.deviceScaleFactor)) {
         console.error('Desktop has non-standard display scaling. Notifying user and disabling all layouts functionality.');
 
-        const errorMessage =
-            `OpenFin Layouts will only work with monitors that are set to a scaling ratio of 100%. This can be changed in monitor or display settings.
-            \n\nPlease contact support@openfin.co with any further questions.`;
+        const errorMessage = `\
+OpenFin Layouts will only work with monitors that are set to a scaling ratio of 100%, 150% or 200%. \
+This can be changed in monitor or display settings.
+\n\n\
+Please contact support@openfin.co with any further questions.`;
 
         const providerChannel: ChannelProvider = await fin.InterApplicationBus.Channel.create(SERVICE_CHANNEL);
         providerChannel.onConnection((app: Identity) => {

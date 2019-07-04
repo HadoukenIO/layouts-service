162fa84ce70fffabc7c6caf7f0f6de5a25009177
Theo Ottah <48205674+TheoOttahOF@users.noreply.github.com>
2019-07-04 13:25:32 +0100

    [TEST] Run only preview tests

--- jest-int.config.js
+++ jest-int.config.js
@@ -1,5 +1,6 @@
 const createConfig = require('./jest-default-config');
-config = createConfig('int');
+const config = createConfig('int');
+config['testRegex'] = 'preview[a-zA-Z]+.inttest.ts';
 config['setupFilesAfterEnv'] = ['<rootDir>/provider/utils/setup.ts'];
 config['testEnvironment'] = '<rootDir>/demo/utils/integrationTestEnv.js';
 

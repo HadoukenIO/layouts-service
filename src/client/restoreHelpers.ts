186bbee83a68dd094387d58d5dcb23c6213dd679
tomrobertsOF <38854624+tomrobertsOF@users.noreply.github.com>
2019-07-26 15:32:14 +0100

    [SERVICE-535] Create restore helper function (#417)

---
+++ src/client/restoreHelpers.ts
@@ -0,0 +1,135 @@
+/**
+ * @module RestoreHelpers
+ */
+import {WorkspaceWindow, WorkspaceApp} from './workspaces';
+
+/**
+ * A simple restore handler that can be passed to {@link setRestoreHandler} when no custom logic is needed.
+ *
+ * This restore handler will open any child windows which are not currently running,
+ * and move new and existing child windows to their expected positions for the workspace being restored.
+ *
+ * ``` ts
+ * import {workspaces, restoreHelpers} from 'openfin-layouts';
+ *
+ * workspaces.setRestoreHandler(restoreHelpers.standardRestoreHandler);
+ * ```
+ *
+ * @param workspaceApp Describes the running app's child windows and their expected post-restore state.
+ * See {@link WorkspaceApp} and {@link setRestoreHandler} for more information on what this will contain.
+ */
+export async function standardRestoreHandler(workspaceApp: WorkspaceApp) {
+    const errors: Error[] = [];
+
+    const openWindows = await fin.Application.getCurrentSync().getChildWindows();
+
+    // Iterate through the child windows of the WorkspaceApp
+    const opened = workspaceApp.childWindows.map(async (win: WorkspaceWindow): Promise<void> => {
+        // Check if the window already exists
+        if (!openWindows.some(w => w.identity.name === win.name)) {
+            // Create the OpenFin window based on the data in the workspaceApp
+            return createChild(win).catch((e: Error) => {
+                errors.push(e);
+            });
+        } else {
+            // Only position if the window exists
+            return positionChild(win).catch((e: Error) =>{
+                errors.push(e);
+            });
+        }
+    });
+
+    // Wait for all windows to open and be positioned before returning
+    await Promise.all(opened);
+    if (errors.length === 0) {
+        return workspaceApp;
+    } else {
+        // Consolidate any errors in restoring the children into a single error
+        throw new Error('One or more errors encountered when restoring children: ' + errors.map(e => e.message).join(', '));
+    }
+}
+
+/**
+ * An additional helper function that can be used to create custom restore handlers.
+ *
+ * Given a {@link WorkspaceWindow} it will determine if the window is open and then
+ * call {@link createChild} or {@link positionChild} as appropriate.
+ *
+ * @param workspaceWindow Object containing details of the window identity and how it should
+ * be positioned/created.
+ */
+export async function createOrPositionChild(workspaceWindow: WorkspaceWindow) {
+    const openWindows = await fin.Application.getCurrentSync().getChildWindows();
+
+    if (!openWindows.some(w => w.identity.name === workspaceWindow.name)) {
+        // create the OpenFin window based on the data in the workspaceApp
+        return createChild(workspaceWindow);
+    } else {
+        // only position if the window exists
+        return positionChild(workspaceWindow);
+    }
+}
+
+/**
+ * Given a {@link WorkspaceWindow} for a non-open window, this function will create the window, position it,
+ * and update its visibility, state, and frame to fit with the restoring workspace.
+ *
+ * Called by the {@link standardRestoreHandler} for any non-open windows in the WorkspaceApp.
+ *
+ * @param workspaceWindow Object containing details of the window identity and how it should
+ * be created.
+ *
+ * @throws `Error`: If there is an existing child window with the same name.
+ */
+export async function createChild(workspaceWindow: WorkspaceWindow): Promise<void> {
+    const {bounds, frame, isShowing, name, state, url} = workspaceWindow;
+    await fin.Window.create({
+        url,
+        autoShow: isShowing,
+        defaultHeight: bounds.height,
+        defaultWidth: bounds.width,
+        defaultLeft: bounds.left,
+        defaultTop: bounds.top,
+        frame,
+        state,
+        name
+    });
+}
+
+/**
+ * Given a {@link WorkspaceWindow} object for a currently open window, this will position and
+ * update the visibility, state and frame of that window to fit with the restoring
+ * workspace.
+ *
+ * Called by the {@link standardRestoreHandler} for any currently running windows in the workspace.
+ *
+ * @param workspaceWindow Object containing details of the window identity and how it should
+ * be positioned.
+ *
+ * @throws `Error`: If the window described by `workspaceWindow` is not currently running.
+ */
+export async function positionChild(workspaceWindow: WorkspaceWindow): Promise<void> {
+    const {bounds, frame, isShowing, state, isTabbed} = workspaceWindow;
+
+    const ofWin = fin.Window.wrapSync(workspaceWindow);
+    await ofWin.setBounds(bounds);
+
+    if (isTabbed) {
+        return;
+    }
+
+    if (!isShowing) {
+        await ofWin.hide();
+        return;
+    }
+
+    if (state === 'normal') {
+        await ofWin.restore();
+    } else if (state === 'minimized') {
+        await ofWin.minimize();
+    } else if (state === 'maximized') {
+        await ofWin.maximize();
+    }
+
+    return ofWin.updateOptions({frame});
+}
--- test/demo/previewWindow.inttest.ts
+++ test/demo/previewWindow.inttest.ts
@@ -38,7 +38,7 @@ itParameterized(
         const {windows} = context;
         const {side} = testOptions;
 
-        const previewWin: _Window = await fin.Window.wrap({name: `preview-${PreviewType.SNAP}-${Validity.VALID}`, uuid: 'layouts-service'});
+        const previewWin: _Window = fin.Window.wrapSync({name: `preview-${PreviewType.SNAP}-${Validity.VALID}`, uuid: 'layouts-service'});
         const windowBounds = await Promise.all([getBounds(windows[0]), getBounds(windows[1])]);
 
         await dragSideToSide(windows[1], opposite(side), windows[0], side, {x: 5, y: 5}, false);
@@ -73,7 +73,7 @@ itParameterized(
         const {dimension, direction} = testOptions;
         const {windows} = context;
 
-        const previewWin: _Window = await fin.Window.wrap({name: `preview-${PreviewType.SNAP}-${Validity.VALID}`, uuid: 'layouts-service'});
+        const previewWin: _Window = fin.Window.wrapSync({name: `preview-${PreviewType.SNAP}-${Validity.VALID}`, uuid: 'layouts-service'});
         const windowBounds = await Promise.all([getBounds(windows[0]), getBounds(windows[1])]);
 
         await windows[1].resizeBy(
@@ -104,7 +104,7 @@ itParameterized(
         const {windowCount} = testOptions;
         const {windows} = context;
 
-        const previewWin: _Window = await fin.Window.wrap({name: `preview-${PreviewType.TAB}-${Validity.VALID}`, uuid: 'layouts-service'});
+        const previewWin: _Window = fin.Window.wrapSync({name: `preview-${PreviewType.TAB}-${Validity.VALID}`, uuid: 'layouts-service'});
         const windowBounds = await Promise.all([getBounds(windows[0]), getBounds(windows[1])]);
 
         if (windowCount > 2) {
@@ -133,7 +133,7 @@ itParameterized(
         const {windowCount} = testOptions;
         const {windows} = context;
 
-        const previewWin: _Window = await fin.Window.wrap({name: `preview-${PreviewType.TAB}-${Validity.VALID}`, uuid: 'layouts-service'});
+        const previewWin: _Window = fin.Window.wrapSync({name: `preview-${PreviewType.TAB}-${Validity.VALID}`, uuid: 'layouts-service'});
 
         await windows[0].moveTo(40, 40);
         if (windowCount > 3) await windows[3].moveTo(60, 60);
